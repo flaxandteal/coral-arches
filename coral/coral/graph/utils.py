0029b10b9b92d9a10ec20cfbd1f1be0bc8af460f
Phil Weir <phil.weir@flaxandteal.co.uk>
2023-03-07 14:55:15 +0000

    cover edge cases for studly/camel/snake

--- coral/coral/graph/utils.py
+++ coral/coral/graph/utils.py
@@ -4,7 +4,9 @@ import slugify
 
 def snake(class_name):
     class_name = class_name[0].lower() + class_name[1:]
-    class_name = re.sub("([A-Z])", r"_\1", class_name)
+    if class_name[0] == "_" and class_name[1].isnumeric():
+        class_name = class_name[1:]
+    class_name = class_name[0] + re.sub("([a-zA-Z])([A-Z])", r"\1_\2", class_name[1:])
     return class_name.lower()
 
 
@@ -19,5 +21,7 @@ def camel(string, studly=False):
     string = ((string[0].upper() if studly else string[0].lower()) + string.replace("_", " ").title()[1:]).replace(" ", "")
     if string[-1] != string[-1].lower():
         string += "_"
+    if string[0].isnumeric():
+        string = "_" + string
     return string
 studly = partial(camel, studly=True)
