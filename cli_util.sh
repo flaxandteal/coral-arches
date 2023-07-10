7be13e21b8d7b652c5979c8fed3ed34048386eb5
Taylor North <taylor@galviadigital.com>
2023-07-10 11:14:53 +0100

    utility-commands

---
+++ cli_util.sh
@@ -0,0 +1,3 @@
+alias rbwebpack="rm -f log && docker exec -ti coral-arches_arches_1 /bin/sh -c '. ../ENV/bin/activate; cd coral; DJANGO_MODE=DEV NODE_PATH=./media/node_modules NODE_OPTIONS=--max_old_space_size=8192 node --inspect ./media/node_modules/.bin/webpack --config webpack/webpack.config.dev.js' 2>&1 > log"
+
+alias djmang='docker exec -it coral-arches_arches_1 bash -c "source ../ENV/bin/activate && python manage.py \"\${0:-help}\" \"\$@\""'
