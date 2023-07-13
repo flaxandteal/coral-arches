e60d5714bb9389dce8a24a263da2d5bf463ddfde
Taylor North <taylor@galviadigital.com>
2023-07-13 15:13:01 +0100

    auto-generate-id-widget

---
+++ coral/coral/media/js/views/components/widgets/auto-generate-id-widget.js
@@ -0,0 +1,38 @@
+define([
+  'knockout',
+  'arches',
+  'uuid',
+  'underscore',
+  'viewmodels/widget',
+  'templates/views/components/widgets/auto-generate-id-widget.htm'
+], function (ko, arches, uuid, _, WidgetViewModel, autoGenerateIdWidget) {
+  /**
+   * registers a text-widget component for use in forms
+   * @function external:"ko.components".text-widget
+   * @param {object} params
+   * @param {string} params.value - the value being managed
+   * @param {function} params.config - observable containing config object
+   * @param {string} params.config().label - label to use alongside the text input
+   * @param {string} params.config().placeholder - default text to show in the text input
+   */
+  return ko.components.register('auto-generate-id-widget', {
+    viewModel: function (params) {
+      params.configKeys = ['id_placeholder', 'label', 'disabled'];
+      WidgetViewModel.apply(this, [params]);
+
+      const self = this;
+      self.idValue = self.value()[arches.activeLanguage]?.value; 
+  
+      if (ko.isObservable(self.idValue) && !self.idValue()) {
+        self.idValue = ko.observable(uuid.generate());
+        self.value({
+          [arches.activeLanguage]: {
+            value: self.idValue,
+            direction: 'ltr'
+          }
+        });
+      }
+    },
+    template: autoGenerateIdWidget
+  });
+});
