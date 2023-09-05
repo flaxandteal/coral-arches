c53c6a0e537eef862058da58719ee64cf6b7eba9
Owen <owen@galviadigital.com>
2023-09-05 09:22:59 +0100

    commiting collecting information step

---
+++ coral/coral/media/js/views/components/workflows/excavation-workflow/collecting-information-step.js
@@ -0,0 +1,23 @@
+ define([
+    'underscore',
+    'knockout',
+    'uuid',
+    'arches',
+    'viewmodels/alert',
+    'templates/views/components/workflows/excavation-workflow/collecting-information-step.htm',
+], function(_, ko, uuid, arches, AlertViewModel, excavationInformationStepTemplate) {
+    function viewModel(params) {
+        // this.resValue = ko.observable().extend({ deferred: true });
+        this.loading = params.loading;
+        this.graphid = params.graphid;
+
+        // params.form.save = function() {
+        // };
+    }
+    ko.components.register('collecting-information-step', {
+        viewModel: viewModel,
+        template: excavationInformationStepTemplate
+    });
+
+    return viewModel;
+});
