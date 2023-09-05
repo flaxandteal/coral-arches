3138960a5691bec9cf79d5ae89c1a81d61e11a5a
Owen <owen@galviadigital.com>
2023-09-05 14:05:30 +0100

    multi label card created. labels need to match the number of visible widgets and be in order.

--- coral/coral/media/js/views/components/workflows/excavation-workflow/multi-widget-with-labels.js
+++ coral/coral/media/js/views/components/workflows/excavation-workflow/multi-widget-with-labels.js
@@ -17,6 +17,13 @@ define([
         console.log("CARD", this.card.widgets())
         console.log("CARD", this.card.widgets()[0].widgetList()[0].name)
         this.widget_group = this.card.widgets()
+        this.labelled_group = this.widget_group.filter((widg) => widg.visible())
+        this.widget_group.forEach((widg, idx) => {
+            widg['selected_label'] = this.labels[idx]
+        });
+        console.log("widg group", this.widget_group)
+        console.log("group with labels", this.labelled_group)
+
         console.log(this.card)
         _.extend(this, params.form);
 
