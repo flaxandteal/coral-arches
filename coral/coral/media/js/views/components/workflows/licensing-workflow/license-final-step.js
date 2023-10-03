6921a9d1bf1e4773da7b0f0168da05a4cd745b04
Taylor North <taylor@galviadigital.com>
2023-10-03 11:35:07 +0100

    moved-summary-logic-to-summary-step

--- coral/coral/media/js/views/components/workflows/licensing-workflow/license-final-step.js
+++ coral/coral/media/js/views/components/workflows/licensing-workflow/license-final-step.js
@@ -1,19 +1,15 @@
 define([
   'knockout',
-  'arches',
   'views/components/workflows/summary-step',
-  'templates/views/components/workflows/licensing-workflow/license-final-step.htm',
-  'uuid'
-], function (ko, arches, SummaryStep, licenseFinalStepTemplate, uuid) {
+  'templates/views/components/workflows/licensing-workflow/license-final-step.htm'
+], function (ko, SummaryStep, licenseFinalStepTemplate) {
   function viewModel(params) {
     SummaryStep.apply(this, [params]);
-    var self = this;
+    // var this = this;
     console.log(SummaryStep);
 
-    self.loading(false);
-
-    self.displayName = 'Temp Display Name';
-    self.applicationId = 'Temp Application ID';
+    this.displayName = 'Temp Display Name';
+    this.applicationId = 'Temp Application ID';
 
     /**
      * {
@@ -28,7 +24,7 @@ define([
      * }
      */
 
-    self.licenseNodes = ko.observable({
+    this.licenseNodes = {
       id: 'license',
       contacts: {
         label: 'Contacts',
@@ -42,14 +38,12 @@ define([
       dates: {
         label: 'Dates',
         nodegroupId: '05f6b846-5d49-11ee-911e-0242ac130003',
-        renderNodeIds: null,
-        data: []
+        renderNodeIds: null
       },
       status: {
         label: 'Application Status',
         nodegroupId: 'ee5947c6-48b2-11ee-abec-0242ac140007',
-        renderNodeIds: null,
-        data: []
+        renderNodeIds: null
       },
       decisionMadeBy: {
         label: 'Decision',
@@ -59,14 +53,12 @@ define([
           '25f04f6c-48cd-11ee-94b3-0242ac140007',
           'f3dcbf02-48cb-11ee-9081-0242ac140007',
           'f6c207ae-5938-11ee-9e74-0242ac130007'
-        ],
-        data: []
+        ]
       },
       systemRef: {
         label: 'System Reference',
         nodegroupId: '991c3c74-48b6-11ee-85af-0242ac140007',
-        renderNodeIds: ['991c49b2-48b6-11ee-85af-0242ac140007'],
-        data: []
+        renderNodeIds: ['991c49b2-48b6-11ee-85af-0242ac140007']
       },
       applicationDetails: {
         label: 'Application Details',
@@ -74,22 +66,17 @@ define([
         renderNodeIds: [
           'aec103a2-48cf-11ee-8e4e-0242ac140007',
           'c2f40174-5dd5-11ee-ae2c-0242ac120008'
-        ],
-        data: []
+        ]
       },
       externalRef: {
         label: 'External Reference',
         nodegroupId: '280b6cfc-4e4d-11ee-a340-0242ac140007',
-        renderNodeIds: [
-          { nodeId: '280b75bc-4e4d-11ee-a340-0242ac140007', label: 'License Number' }
-        ],
-        data: []
+        renderNodeIds: [{ nodeId: '280b75bc-4e4d-11ee-a340-0242ac140007', label: 'License Number' }]
       },
       excavationType: {
         label: 'Excavation Type',
         nodegroupId: '6e071042-5d45-11ee-88b0-0242ac120008',
-        renderNodeIds: ['6e071042-5d45-11ee-88b0-0242ac120008'],
-        data: []
+        renderNodeIds: ['6e071042-5d45-11ee-88b0-0242ac120008']
       },
       communication: {
         label: 'Email',
@@ -98,30 +85,26 @@ define([
           '684110e4-48ce-11ee-8e4e-0242ac140007',
           '68410b3a-48ce-11ee-8e4e-0242ac140007',
           { nodeId: '684113a0-48ce-11ee-8e4e-0242ac140007', defaultValue: 'None Provided' }
-        ],
-        data: []
+        ]
       },
       associatedActivities: {
         label: 'Associated Activities',
         nodegroupId: 'a9f53f00-48b6-11ee-85af-0242ac140007',
-        renderNodeIds: ['a9f53f00-48b6-11ee-85af-0242ac140007'],
-        data: []
+        renderNodeIds: ['a9f53f00-48b6-11ee-85af-0242ac140007']
       },
       digitalFiles: {
         label: 'Digital Files',
         nodegroupId: '8c5356f4-48ce-11ee-8e4e-0242ac140007',
-        renderNodeIds: ['8c5356f4-48ce-11ee-8e4e-0242ac140007'],
-        data: []
+        renderNodeIds: ['8c5356f4-48ce-11ee-8e4e-0242ac140007']
       }
-    });
+    };
 
-    self.activityNodes = ko.observable({
+    this.activityNodes = {
       id: 'activity',
       name: {
         label: 'Activity',
         nodegroupId: '4a7bba1d-9938-11ea-86aa-f875a44e0e11',
-        renderNodeIds: ['4a7be135-9938-11ea-b0e2-f875a44e0e11'],
-        data: []
+        renderNodeIds: ['4a7be135-9938-11ea-b0e2-f875a44e0e11']
       },
       systemRef: {
         label: 'System Reference',
@@ -131,8 +114,7 @@ define([
           'e7d69604-9939-11ea-baef-f875a44e0e11'
           // { nodeId: 'e7d69603-9939-11ea-9e7f-f875a44e0e11', label: 'Application ID' },
           // { nodeId: 'e7d69604-9939-11ea-baef-f875a44e0e11', label: 'Planning Reference' }
-        ],
-        data: []
+        ]
       },
       externalRef: {
         label: 'External References',
@@ -140,14 +122,12 @@ define([
         renderNodeIds: [
           '589d4dc7-edf9-11eb-9856-a87eeabdefba',
           '589d4dca-edf9-11eb-83ea-a87eeabdefba'
-        ],
-        data: []
+        ]
       },
       digitalFiles: {
         label: 'Digital Files',
         nodegroupId: '316c7d1e-8554-11ea-aed7-f875a44e0e11',
-        renderNodeIds: ['316c7d1e-8554-11ea-aed7-f875a44e0e11'],
-        data: []
+        renderNodeIds: ['316c7d1e-8554-11ea-aed7-f875a44e0e11']
       },
       areaType: {
         label: 'Area',
@@ -155,8 +135,7 @@ define([
         renderNodeIds: [
           'a5416b53-f121-11eb-a507-a87eeabdefba',
           'a541922e-f121-11eb-b2f6-a87eeabdefba'
-        ],
-        data: []
+        ]
       },
       address: {
         label: 'Address',
@@ -173,18 +152,16 @@ define([
           'a541b927-f121-11eb-8377-a87eeabdefba',
           'a541b925-f121-11eb-9264-a87eeabdefba',
           'a541b922-f121-11eb-9fa2-a87eeabdefba'
-        ],
-        data: []
+        ]
       },
       digitalFiles: {
         label: 'Digital Files',
         nodegroupId: '316c7d1e-8554-11ea-aed7-f875a44e0e11',
-        renderNodeIds: ['316c7d1e-8554-11ea-aed7-f875a44e0e11'],
-        data: []
+        renderNodeIds: ['316c7d1e-8554-11ea-aed7-f875a44e0e11']
       }
-    });
+    };
 
-    self.digitalFilesNodes = {
+    this.digitalFilesNodes = {
       id: 'digital-files',
       name: {
         label: 'Digital Object',
@@ -198,192 +175,22 @@ define([
       }
     };
 
-    self.renderedNodegroups = ko.observable({});
+    this.getData = async () => {
+      await this.renderResourceIds(this.resourceid, this.licenseNodes);
 
-    self.renderNode = async (key, tileData, config) => {
-      console.log('tileData: ', tileData);
-      console.log('key: ', key);
-      let result = [];
-      console.log('config: ', config);
-      const tiles = tileData[config?.nodegroupId];
-      console.log('tiles: ', tiles);
+      let digitalFileResourceIds = this.getResourceIds(this.licenseNodes.digitalFiles);
+      await this.renderResourceIds(digitalFileResourceIds, this.digitalFilesNodes);
 
-      const getNodeDataFromTile = async (t) => {
-        console.log('t: ', JSON.stringify(t));
-        const nodeData = [];
-        if (!config.renderNodeIds) {
-          // If no render nodes specified render them all
-          nodeData.push(Object.values(t.data));
-        } else {
-          const filtered = [];
-          for await (const nodeIdObject of config.renderNodeIds) {
-            if (!(nodeIdObject in t.data)) continue;
-            if (typeof nodeIdObject === 'string' || nodeIdObject instanceof String) {
-              // Use the node ID string to get the node object
-              filtered.push(t.data[nodeIdObject]);
-            } else {
-              /**
-               * This is the object notation path that allows you to
-               * provide values such as: label, defaultValue, related
-               */
-              const node = t.data[nodeIdObject.nodeId];
-              console.log('nodeIdObject: ', nodeIdObject);
-              if (nodeIdObject.label) {
-                node.label = nodeIdObject.label;
-              }
-              if (nodeIdObject.defaultValue && !node.displayValue) {
-                node.displayValue = nodeIdObject.defaultValue;
-              }
-              filtered.push(node);
-            }
-          }
-          if (filtered.length) {
-            nodeData.push(filtered);
-          }
-        }
-        return nodeData;
-      };
+      const activityResourceIds = this.getResourceIds(this.licenseNodes.associatedActivities);
+      await this.renderResourceIds(activityResourceIds, this.activityNodes);
 
-      if (tiles) {
-        if (Array.isArray(tiles)) {
-          // Multi Tile Node
-          for await (const tile of tiles) {
-            result = [...result, ...(await getNodeDataFromTile(tile))];
-          }
-        } else {
-          // Single Tile Node
-          result = [...result, ...(await getNodeDataFromTile(tiles))];
-        }
-      }
+      digitalFileResourceIds = this.getResourceIds(this.activityNodes.digitalFiles);
+      await this.renderResourceIds(digitalFileResourceIds, this.digitalFilesNodes);
 
-      console.log('result: ', result);
-      config.data = result;
-
-      return config;
+      console.log('this.renderedNodegroups: ', this.renderedNodegroups());
     };
 
-    self.renderAllNodes = async (nodeConfigs, tileData) => {
-      for await (const [key, value] of Object.entries(nodeConfigs)) {
-        const result = await self.renderNode(key, tileData, value);
-        nodeConfigs[key] = result;
-      }
-      if (!(nodeConfigs.id in self.renderedNodegroups())) {
-        self.renderedNodegroups()[nodeConfigs.id] = [];
-      }
-      self.renderedNodegroups()[nodeConfigs.id].push(nodeConfigs);
-      return nodeConfigs;
-    };
-
-    self.formatTileData = (tileData) => {
-      const formatted = {};
-      tileData.forEach((tile) => {
-        const cardinality = !!tileData.find(
-          (t) => t.tileid !== tile.tileid && t.nodegroup === tile.nodegroup
-        );
-        let cardinalityIdx = null;
-        if (!cardinality) {
-          formatted[tile.nodegroup] = {
-            nodegroupId: tile.nodegroup,
-            tileId: tile.tileid,
-            data: {}
-          };
-        } else {
-          if (tile.nodegroup in formatted) {
-            formatted[tile.nodegroup].push({
-              nodegroupId: tile.nodegroup,
-              tileId: tile.tileid,
-              data: {}
-            });
-            cardinalityIdx = formatted[tile.nodegroup].length - 1;
-          } else {
-            formatted[tile.nodegroup] = [];
-            formatted[tile.nodegroup].push({
-              nodegroupId: tile.nodegroup,
-              tileId: tile.tileid,
-              data: {}
-            });
-            cardinalityIdx = formatted[tile.nodegroup].length - 1;
-          }
-        }
-        tile.display_values.forEach((display) => {
-          console.log('cardinality: ', cardinality, cardinalityIdx);
-          if (!cardinality) {
-            formatted[tile.nodegroup]['data'][display.nodeid] = {
-              nodeId: display.nodeid,
-              label: display.label,
-              displayValue: display.value,
-              value: tile.data[display.nodeid]
-            };
-          } else {
-            formatted[tile.nodegroup][cardinalityIdx]['data'][display.nodeid] = {
-              nodeId: display.nodeid,
-              label: display.label,
-              displayValue: display.value,
-              value: tile.data[display.nodeid]
-            };
-          }
-        });
-      });
-      return formatted;
-    };
-
-    self.fetchTileData = async (resourceId) => {
-      console.log('Fetching tiles for resource ID: ', resourceId);
-      const tilesResponse = await window.fetch(
-        arches.urls.resource_tiles.replace('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', resourceId)
-      );
-      const data = await tilesResponse.json();
-      console.log('Logging tilesResponse: ', tilesResponse, data.tiles);
-      return data.tiles;
-    };
-
-    self.hasNodeGroup = (tileData, nodeGroupId) => {
-      return tileData.some((tile) => tile.nodegroup === nodeGroupId);
-    };
-
-    self.getResourceIds = (nodeConfig) => {
-      const ids = [];
-      nodeConfig.data.forEach((data) => {
-        data.forEach((node) => {
-          node.value.forEach((relationship) => {
-            ids.push(relationship.resourceId);
-          });
-        });
-      });
-      return ids;
-    };
-
-    self.renderResourceIds = async (ids, nodeConfigs) => {
-      console.log('ids: ', ids);
-      if (!Array.isArray(ids)) {
-        ids = [ids];
-      }
-      for await (const id of ids) {
-        const tiles = await self.fetchTileData(id);
-        const formattedTiles = self.formatTileData(tiles);
-        await self.renderAllNodes(nodeConfigs, formattedTiles);
-      }
-    };
-
-    self.loadData = async () => {
-      self.loading(true);
-      await self.renderResourceIds(self.resourceid, self.licenseNodes());
-
-      let digitalFileResourceIds = self.getResourceIds(self.licenseNodes().digitalFiles);
-      await self.renderResourceIds(digitalFileResourceIds, self.digitalFilesNodes);
-
-      const activityResourceIds = self.getResourceIds(self.licenseNodes().associatedActivities);
-      await self.renderResourceIds(activityResourceIds, self.activityNodes());
-
-      digitalFileResourceIds = self.getResourceIds(self.activityNodes().digitalFiles);
-      await self.renderResourceIds(digitalFileResourceIds, self.digitalFilesNodes);
-
-      console.log('self.renderedNodegroups: ', self.renderedNodegroups());
-      self.loading(false);
-    };
-
-    // Call load data
-    self.loadData();
+    this.loadData();
   }
 
   ko.components.register('license-final-step', {
--- coral/coral/media/js/views/components/workflows/summary-step.js
+++ coral/coral/media/js/views/components/workflows/summary-step.js
@@ -1,75 +1,277 @@
 define([
-    'knockout',
-    'views/components/workflows/final-step',
-    'geojson-extent',
-    'views/components/map',
-    'views/components/cards/select-feature-layers',
-    'viewmodels/alert'
-], function(ko, FinalStep, geojsonExtent, MapComponentViewModel, selectFeatureLayersFactory, AlertViewModel) {
+  'knockout',
+  'views/components/workflows/final-step',
+  'geojson-extent',
+  'arches',
+  'views/components/map',
+  'views/components/cards/select-feature-layers',
+  'viewmodels/alert'
+], function (
+  ko,
+  FinalStep,
+  geojsonExtent,
+  arches,
+  MapComponentViewModel,
+  selectFeatureLayersFactory,
+  AlertViewModel
+) {
+  function viewModel(params) {
+    FinalStep.apply(this, [params]);
+    this.resourceData = ko.observable();
+    this.relatedResources = ko.observableArray();
+    this.loading = ko.observable(false);
 
-    function viewModel(params) {
-        FinalStep.apply(this, [params]);
-        this.resourceData = ko.observable();
-        this.relatedResources = ko.observableArray();
+    this.getResourceData = function () {
+      window
+        .fetch(this.urls.api_resources(this.resourceid) + '?format=json&compact=false')
+        .then((response) => response.json())
+        .then((data) => this.resourceData(data));
+    };
 
-        this.getResourceData = function() {
-            window.fetch(this.urls.api_resources(this.resourceid) + '?format=json&compact=false')
-            .then(response => response.json())
-            .then(data => this.resourceData(data))
-        };
+    this.getRelatedResources = function () {
+      window
+        .fetch(this.urls.related_resources + this.resourceid + '?paginate=false')
+        .then((response) => response.json())
+        .then((data) => this.relatedResources(data));
+    };
 
-        this.getRelatedResources = function() {
-            window.fetch(this.urls.related_resources + this.resourceid + "?paginate=false")
-            .then(response => response.json())
-            .then(data => this.relatedResources(data))
-        };
+    this.init = async () => {
+      this.getResourceData();
+      this.getRelatedResources();
+    };
 
-        this.init = function(){
-            this.getResourceData();
-            this.getRelatedResources()
-        };
+    this.getResourceValue = function (obj, attrs, missingValue = 'none') {
+      try {
+        return (
+          attrs.reduce(function index(obj, i) {
+            return obj[i];
+          }, obj) || missingValue
+        );
+      } catch (e) {
+        return missingValue;
+      }
+    };
 
-        this.getResourceValue = function(obj, attrs, missingValue='none') {
-            try {
-                return attrs.reduce(function index(obj, i) {return obj[i]}, obj) || missingValue;
-            } catch(e) {
-                return missingValue;
+    this.prepareMap = function (geojson, source) {
+      var mapParams = {};
+      if (geojson.features.length > 0) {
+        mapParams.bounds = geojsonExtent(geojson);
+        mapParams.fitBoundsOptions = { padding: 20 };
+      }
+      var sourceConfig = {};
+      sourceConfig[source] = {
+        type: 'geojson',
+        data: geojson
+      };
+      mapParams.sources = Object.assign(sourceConfig, mapParams.sources);
+      mapParams.layers = selectFeatureLayersFactory(
+        '', //resourceid
+        source, //source
+        undefined, //sourceLayer
+        [], //selectedResourceIds
+        true, //visible
+        '#ff2222' //color
+      );
+      MapComponentViewModel.apply(this, [
+        Object.assign({}, mapParams, {
+          activeTab: ko.observable(false),
+          zoom: null
+        })
+      ]);
+
+      this.layers = mapParams.layers;
+      this.sources = mapParams.sources;
+    };
+
+    this.renderedNodegroups = ko.observable({});
+
+    this.renderNode = async (key, tileData, config) => {
+      console.log('tileData: ', tileData);
+      console.log('key: ', key);
+      let result = [];
+      console.log('config: ', config);
+      const tiles = tileData[config?.nodegroupId];
+      console.log('tiles: ', tiles);
+
+      const getNodeDataFromTile = async (t) => {
+        console.log('t: ', JSON.stringify(t));
+        const nodeData = [];
+        if (!config.renderNodeIds) {
+          // If no render nodes specified render them all
+          console.log('nodeData: ', nodeData, Object.values(t.data));
+          nodeData.push(Object.values(t.data));
+        } else {
+          const filtered = [];
+          for await (const nodeIdObject of config.renderNodeIds) {
+            if (!(nodeIdObject in t.data)) continue;
+            if (typeof nodeIdObject === 'string' || nodeIdObject instanceof String) {
+              // Use the node ID string to get the node object
+              const node = t.data[nodeIdObject];
+              if (node.displayValue) {
+                filtered.push(node);
+              }
+              continue;
             }
-        };
-
-        this.prepareMap = function(geojson, source) {
-            var mapParams = {};
-            if (geojson.features.length > 0) {
-                mapParams.bounds = geojsonExtent(geojson);
-                mapParams.fitBoundsOptions = { padding: 20 };
+            /**
+             * This is the object notation path that allows you to
+             * provide values such as: label, defaultValue, related
+             */
+            const node = t.data[nodeIdObject.nodeId];
+            console.log('nodeIdObject: ', nodeIdObject);
+            if (nodeIdObject.label) {
+              node.label = nodeIdObject.label;
             }
-            var sourceConfig = {};
-            sourceConfig[source] = {
-                    "type": "geojson",
-                    "data": geojson
-                };
-            mapParams.sources = Object.assign(sourceConfig, mapParams.sources);
-            mapParams.layers = selectFeatureLayersFactory(
-                '', //resourceid
-                source, //source
-                undefined, //sourceLayer
-                [], //selectedResourceIds
-                true, //visible
-                '#ff2222' //color
-            );
-            MapComponentViewModel.apply(this, [Object.assign({},  mapParams,
-                {
-                    "activeTab": ko.observable(false),
-                    "zoom": null
-                }
-            )]);
-        
-            this.layers = mapParams.layers;
-            this.sources = mapParams.sources;
-        };
+            if (nodeIdObject.defaultValue && !node.displayValue) {
+              node.displayValue = nodeIdObject.defaultValue;
+            }
+            filtered.push(node);
+          }
+          if (filtered.length) {
+            nodeData.push(filtered);
+          }
+        }
 
-        this.init();
-    }
-    
-    return viewModel;
+        console.log('nodeData: ', nodeData);
+        return nodeData;
+      };
+
+      if (tiles) {
+        if (Array.isArray(tiles)) {
+          // Multi Tile Node
+          for await (const tile of tiles) {
+            const temp = await getNodeDataFromTile(tile);
+            console.log('temppppp: ', temp);
+            result = [...result, ...(await getNodeDataFromTile(tile))];
+          }
+        } else {
+          // Single Tile Node
+          const temp = await getNodeDataFromTile(tiles);
+          console.log('temppppp: ', temp);
+          result = [...result, ...(await getNodeDataFromTile(tiles))];
+        }
+      }
+
+      console.log('result: ', result);
+      config.data = result;
+
+      return config;
+    };
+
+    this.renderAllNodes = async (nodeConfigs, tileData) => {
+      for await (const [key, value] of Object.entries(nodeConfigs)) {
+        const result = await this.renderNode(key, tileData, value);
+        nodeConfigs[key] = result;
+      }
+      if (!(nodeConfigs.id in this.renderedNodegroups())) {
+        this.renderedNodegroups()[nodeConfigs.id] = [];
+      }
+      this.renderedNodegroups()[nodeConfigs.id].push(nodeConfigs);
+      return nodeConfigs;
+    };
+
+    this.formatTileData = (tileData) => {
+      const formatted = {};
+      tileData.forEach((tile) => {
+        const cardinality = !!tileData.find(
+          (t) => t.tileid !== tile.tileid && t.nodegroup === tile.nodegroup
+        );
+        let cardinalityIdx = null;
+        if (!cardinality) {
+          formatted[tile.nodegroup] = {
+            nodegroupId: tile.nodegroup,
+            tileId: tile.tileid,
+            data: {}
+          };
+        } else {
+          if (tile.nodegroup in formatted) {
+            formatted[tile.nodegroup].push({
+              nodegroupId: tile.nodegroup,
+              tileId: tile.tileid,
+              data: {}
+            });
+            cardinalityIdx = formatted[tile.nodegroup].length - 1;
+          } else {
+            formatted[tile.nodegroup] = [];
+            formatted[tile.nodegroup].push({
+              nodegroupId: tile.nodegroup,
+              tileId: tile.tileid,
+              data: {}
+            });
+            cardinalityIdx = formatted[tile.nodegroup].length - 1;
+          }
+        }
+        tile.display_values.forEach((display) => {
+          console.log('cardinality: ', cardinality, cardinalityIdx);
+          if (!cardinality) {
+            formatted[tile.nodegroup]['data'][display.nodeid] = {
+              nodeId: display.nodeid,
+              label: display.label,
+              displayValue: display.value,
+              value: tile.data[display.nodeid]
+            };
+          } else {
+            formatted[tile.nodegroup][cardinalityIdx]['data'][display.nodeid] = {
+              nodeId: display.nodeid,
+              label: display.label,
+              displayValue: display.value,
+              value: tile.data[display.nodeid]
+            };
+          }
+        });
+      });
+      return formatted;
+    };
+
+    this.fetchTileData = async (resourceId) => {
+      console.log('Fetching tiles for resource ID: ', resourceId);
+      const tilesResponse = await window.fetch(
+        arches.urls.resource_tiles.replace('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', resourceId)
+      );
+      const data = await tilesResponse.json();
+      console.log('Logging tilesResponse: ', tilesResponse, data.tiles);
+      return data.tiles;
+    };
+
+    this.hasNodeGroup = (tileData, nodeGroupId) => {
+      return tileData.some((tile) => tile.nodegroup === nodeGroupId);
+    };
+
+    this.getResourceIds = (nodeConfig) => {
+      const ids = [];
+      nodeConfig.data.forEach((data) => {
+        data.forEach((node) => {
+          node.value.forEach((relationship) => {
+            ids.push(relationship.resourceId);
+          });
+        });
+      });
+      return ids;
+    };
+
+    this.renderResourceIds = async (ids, nodeConfigs) => {
+      console.log('ids: ', ids);
+      if (!Array.isArray(ids)) {
+        ids = [ids];
+      }
+      for await (const id of ids) {
+        const tiles = await this.fetchTileData(id);
+        const formattedTiles = this.formatTileData(tiles);
+        await this.renderAllNodes(nodeConfigs, formattedTiles);
+      }
+    };
+
+    this.getData = async () => {
+      console.log('summary-step.js: Please configure the getData function');
+    };
+
+    this.loadData = async () => {
+      this.loading(true);
+      await this.getData();
+      this.loading(false);
+    };
+
+    this.init();
+  }
+
+  return viewModel;
 });
