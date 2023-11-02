1bf7bb6a3d33c15c51f35ce744b98281df7d52de
Taylor North <taylor@galviadigital.com>
2023-11-02 14:36:44 +0000

    WIP Planning consultation response workflow

--- coral/coral/media/js/views/components/plugins/hm-consultation-response-workflow.js
+++ coral/coral/media/js/views/components/plugins/hm-consultation-response-workflow.js
@@ -3,7 +3,7 @@ define([
   'arches',
   'viewmodels/editable-workflow',
   'templates/views/components/plugins/hm-consultation-response-workflow.htm',
-  'views/components/workflows/select-resource-step'
+  'views/components/workflows/planning-consultation/consultation-select-step'
 ], function (ko, arches, EditableWorkflow, workflowTemplate) {
   return ko.components.register('hm-consultation-response-workflow', {
     viewModel: function (params) {
@@ -20,15 +20,49 @@ define([
             {
               componentConfigs: [
                 {
-                  componentName: 'select-resource-step',
+                  componentName: 'consultation-select-step',
                   uniqueInstanceName: 'select-step',
                   tilesManaged: 'one',
                   parameters: {
-                    graphid: 'cc5da227-24e7-4088-bb83-a564c4331efd'
-                    // nodegroupid: '991c3c74-48b6-11ee-85af-0242ac140007',
+                    graphid: '8effdca4-ffb6-482b-94b8-4d35fb5c88c5',
+                    graphids: ['8effdca4-ffb6-482b-94b8-4d35fb5c88c5'],
+                    nodegroupid: 'c853846a-7948-42c8-a089-63ebe34b49e4'
                     // hiddenNodes: ['991c4340-48b6-11ee-85af-0242ac140007']
                   }
                 }
+              ]
+            }
+          ]
+        },
+        {
+          title: 'Second Step',
+          name: 'second-step',
+          required: true,
+          layoutSections: [
+            {
+              componentConfigs: [
+                {
+                  componentName: 'consultation-select-step',
+                  uniqueInstanceName: 'select-step',
+                  tilesManaged: 'one',
+                  parameters: {
+                    graphid: '8effdca4-ffb6-482b-94b8-4d35fb5c88c5',
+                    graphids: ['8effdca4-ffb6-482b-94b8-4d35fb5c88c5'],
+                    nodegroupid: 'c853846a-7948-42c8-a089-63ebe34b49e4'
+                    // hiddenNodes: ['991c4340-48b6-11ee-85af-0242ac140007']
+                  }
+                }
+                // {
+                //   componentName: 'select-resource-step',
+                //   uniqueInstanceName: 'select-step',
+                //   tilesManaged: 'one',
+                //   parameters: {
+                //     graphid: '8effdca4-ffb6-482b-94b8-4d35fb5c88c5',
+                //     graphids: ['8effdca4-ffb6-482b-94b8-4d35fb5c88c5'],
+                //     nodegroupid: 'c853846a-7948-42c8-a089-63ebe34b49e4'
+                //     // hiddenNodes: ['991c4340-48b6-11ee-85af-0242ac140007']
+                //   }
+                // }
               ]
             }
           ]
