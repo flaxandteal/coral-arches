d74909127f4e4394c9db450ecefe7b7c53417eb1
Phil Weir <phil.weir@flaxandteal.co.uk>
2023-11-19 00:02:47 +0000

    upgrade to re_path

--- coral/coral/urls.py
+++ coral/coral/urls.py
@@ -1,5 +1,6 @@
 from arches.app.views.plugin import PluginView
-from django.conf.urls import include, url
+from django.urls import re_path
+from django.conf.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
 from coral.views.file_template import FileTemplateView
@@ -15,27 +16,27 @@ from coral.views.graph_component_export import GraphComponentExport
 uuid_regex = settings.UUID_REGEX
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='root'),
-    url(r'^'+settings.APP_PATHNAME+'/$', IndexView.as_view(), name='consultations-root'),
-    url(r'^index.htm', IndexView.as_view(), name='home'),
-    url(r'^'+settings.APP_PATHNAME+'/index.htm', IndexView.as_view(), name='consultations-home'),
-    url(r'^'+settings.APP_PATHNAME+'/', include('arches.urls')),
-    url(r'^plugins/active-consultations$', RedirectView.as_view(url='/'+settings.APP_PATHNAME+'/plugins/active-consultations')),
-    url(r'^resource/descriptors/(?P<resourceid>%s|())$' % uuid_regex, ResourceDescriptors.as_view(), name="resource_descriptors"),
-    url(r'^'+settings.APP_PATHNAME+'/index.htm', IndexView.as_view(), name='home'),
-    url(r'^', include('arches.urls')),
-    url(r'^'+settings.APP_PATHNAME+'/user$', UserManagerView.as_view(), name="user_profile_manager"),
-    url(r'^filetemplate', FileTemplateView.as_view(), name='filetemplate'),
-    url(r'^'+settings.APP_PATHNAME+'/plugins/active-consultations', PluginView.as_view(), name='active-consultations'),
-    url(r'^activeconsultations', ActiveConsultationsView.as_view(),
+    re_path(r'^$', IndexView.as_view(), name='root'),
+    re_path(r'^'+settings.APP_PATHNAME+'/$', IndexView.as_view(), name='consultations-root'),
+    re_path(r'^index.htm', IndexView.as_view(), name='home'),
+    re_path(r'^'+settings.APP_PATHNAME+'/index.htm', IndexView.as_view(), name='consultations-home'),
+    re_path(r'^'+settings.APP_PATHNAME+'/', include('arches.urls')),
+    re_path(r'^plugins/active-consultations$', RedirectView.as_view(url='/'+settings.APP_PATHNAME+'/plugins/active-consultations')),
+    re_path(r'^resource/descriptors/(?P<resourceid>%s|())$' % uuid_regex, ResourceDescriptors.as_view(), name="resource_descriptors"),
+    re_path(r'^'+settings.APP_PATHNAME+'/index.htm', IndexView.as_view(), name='home'),
+    re_path(r'^', include('arches.urls')),
+    re_path(r'^'+settings.APP_PATHNAME+'/user$', UserManagerView.as_view(), name="user_profile_manager"),
+    re_path(r'^filetemplate', FileTemplateView.as_view(), name='filetemplate'),
+    re_path(r'^'+settings.APP_PATHNAME+'/plugins/active-consultations', PluginView.as_view(), name='active-consultations'),
+    re_path(r'^activeconsultations', ActiveConsultationsView.as_view(),
         name='activeconsultations'),
-    url(r'^'+settings.APP_PATHNAME+'/plugins/consultation-workflow', PluginView.as_view(), name='consultation-workflow'),
-    url(r'^'+settings.APP_PATHNAME+'/plugins/application-area', PluginView.as_view(), name='application-area'),
-    url(r'^'+settings.APP_PATHNAME+'/plugins/site-visit', PluginView.as_view(), name='site-visit'),
-    url(r'^'+settings.APP_PATHNAME+'/plugins/correspondence-workflow', PluginView.as_view(), name='correspondence-workflow'),
-    url(r'^'+settings.APP_PATHNAME+'/plugins/communication-workflow', PluginView.as_view(), name='communication-workflow'),
-    url(r'^'+settings.APP_PATHNAME+'/plugins/init-workflow', PluginView.as_view(), name='init-workflow'),
-    url(r'^graph-component-export', GraphComponentExport.as_view(), name='graph-mapping-dump'),
+    re_path(r'^'+settings.APP_PATHNAME+'/plugins/consultation-workflow', PluginView.as_view(), name='consultation-workflow'),
+    re_path(r'^'+settings.APP_PATHNAME+'/plugins/application-area', PluginView.as_view(), name='application-area'),
+    re_path(r'^'+settings.APP_PATHNAME+'/plugins/site-visit', PluginView.as_view(), name='site-visit'),
+    re_path(r'^'+settings.APP_PATHNAME+'/plugins/correspondence-workflow', PluginView.as_view(), name='correspondence-workflow'),
+    re_path(r'^'+settings.APP_PATHNAME+'/plugins/communication-workflow', PluginView.as_view(), name='communication-workflow'),
+    re_path(r'^'+settings.APP_PATHNAME+'/plugins/init-workflow', PluginView.as_view(), name='init-workflow'),
+    re_path(r'^graph-component-export', GraphComponentExport.as_view(), name='graph-mapping-dump'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # if settings.SHOW_LANGUAGE_SWITCH is True:
