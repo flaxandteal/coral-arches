d604bebefc73ee457d8455ff99acdffa6774cb59
teenorth <taylornorth13@hotmail.com>
2023-12-01 21:55:13 +0000

    Basic selecting base resource to start workflow

---
+++ coral/coral/media/js/views/components/plugins/workflow-builder.js
@@ -0,0 +1,37 @@
+define([
+  'jquery',
+  'knockout',
+  'knockout-mapping',
+  'arches',
+  'templates/views/components/plugins/workflow-builder.htm',
+  'plugins/knockout-select2'
+], function ($, ko, koMapping, arches, pageTemplate) {
+  const pageViewModel = function (params) {
+    this.selectedResource = ko.observable();
+
+    this.selectedResource.subscribe((value) => {
+      console.log('selectedResource: ', value);
+    });
+
+    this.resources = ko.observable();
+
+    this.openWorkflowBuilder = (graphId) => {
+      console.log('graphId: ', graphId);
+    };
+
+    this.init = async () => {
+      const resources = await (
+        await window.fetch(arches.urls.root + `workflow-builder/resources`)
+      ).json();
+      console.log('resources: ', resources);
+      this.resources(resources.resources);
+    };
+
+    this.init();
+  };
+
+  return ko.components.register('workflow-builder', {
+    viewModel: pageViewModel,
+    template: pageTemplate
+  });
+});
