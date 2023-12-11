83f165b634fb8eb3ff2d8fe9baa6a46548e4a069
teenorth <taylornorth13@hotmail.com>
2023-12-11 22:24:46 +0000

    Selectable nodegroups and rendering those nodegroups

--- coral/coral/media/js/viewmodels/workflow-builder-card.js
+++ coral/coral/media/js/viewmodels/workflow-builder-card.js
@@ -4,8 +4,9 @@ define([
   'knockout',
   'knockout-mapping',
   'arches',
-  'templates/views/viewmodels/workflow-builder-card.htm'
-], function ($, _, ko, koMapping, arches, template) {
+  'templates/views/viewmodels/workflow-builder-card.htm',
+  'views/components/workflows/workflow-component-abstract'
+], function ($, _, ko, koMapping, arches, template, WorkflowComponentAbstract) {
   const WorkflowBuilderCard = function (params) {
     _.extend(this, params);
 
@@ -14,12 +15,63 @@ define([
     this.selectedNodegroup = ko.observable();
     this.optionsNodegroups = ko.observableArray(['test', 'test1', 'test3']);
 
+    this.workflowComponentAbstract = ko.observable();
+
     this.selectedNodegroup.subscribe((value) => {
-      console.log(this.title + ' ' + value);
+      const data = JSON.parse(JSON.stringify(value));
+      delete data.parameters.resourceid;
+      console.log(this.title, data);
+
+      this.loadAbstractComponent(data);
     });
 
-    this.init = () => {
+    this.components = ko.observableArray();
+
+    this.isStepActive = ko.observable(false);
+
+    this.loadComponents = async () => {
+      let searchParams = new URLSearchParams(window.location.search);
+      let graphId = searchParams.get('graph-id');
+      const data = await (
+        await window.fetch(
+          arches.urls.root + `workflow-builder/graph-components?graph-id=${graphId}`
+        )
+      ).json();
+      return data.component_configs;
+    };
+
+    this.loadAbstractComponent = (componentData) => {
+      this.isStepActive(false);
+      let workflowCA = new WorkflowComponentAbstract({
+        componentData: componentData,
+        workflowComponentAbstractId: null,
+        isValidComponentPath: () => {
+          console.log('isValidComponentPath was called');
+        },
+        getDataFromComponentPath: () => {
+          console.log('getDataFromComponentPath was called');
+        },
+        title: 'Step title',
+        isStepSaving: false,
+        locked: false,
+        lockExternalStep: () => {
+          console.log('lockExternalStep was called');
+        },
+        lockableExternalSteps: [],
+        workflowId: '1234',
+        alert: null,
+        outerSaveOnQuit: null,
+        isStepActive: this.isStepActive
+      });
+      this.isStepActive(true);
+      this.workflowComponentAbstract(workflowCA);
+      console.log('this.workflowComponentAbstract(): ', this.workflowComponentAbstract());
+    };
+
+    this.init = async () => {
       console.log('workflow-builder-card: ', this, params);
+      this.components(await this.loadComponents());
+      console.log('this.components: ', this.components());
     };
 
     this.init();
