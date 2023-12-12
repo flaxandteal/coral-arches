f65dc30dc18f6063cad8a95f1884274d530c2272
teenorth <taylornorth13@hotmail.com>
2023-12-12 22:06:26 +0000

    Added url for overriding the card endpoint to add fabricated tiles

--- coral/coral/views/workflow_builder.py
+++ coral/coral/views/workflow_builder.py
@@ -1,17 +1,24 @@
 from django.views.generic import View
 from arches.app.utils.response import JSONResponse
-
-# from django.http import HttpRequest, HttpResponse
-import time
-import json
 from arches.app.models.resource import Resource
-from arches.app.models.tile import Tile
 from arches.app.models.graph import Graph
-import copy
-import uuid
-from django.http import HttpRequest, HttpResponse
 from arches.app.utils.permission_backend import get_createable_resource_types
 from arches.app.utils.betterJSONSerializer import JSONSerializer, JSONDeserializer
+from arches.app.views import api
+import json
+import uuid
+from django.views.generic import View
+from django.utils.translation import ugettext as _
+from arches.app.models import models
+from arches.app.models.graph import Graph
+from arches.app.models.resource import Resource
+from arches.app.models.system_settings import settings
+from arches.app.utils.response import JSONResponse
+from arches.app.utils.betterJSONSerializer import JSONSerializer, JSONDeserializer
+from arches.app.utils.permission_backend import (
+    user_is_resource_reviewer,
+)
+from django.utils import translation
 
 
 class WorkflowBuilder(View):
@@ -90,3 +97,151 @@ class WorkflowBuilderGraphComponents(View):
                 "component_configs": component_configs,
             }
         )
+
+
+class WorkflowBuilderCardOverride(api.Card):
+    def get(self, request, resourceid):
+        try:
+            resource_instance = Resource.objects.get(pk=resourceid)
+            graph = resource_instance.graph
+        except Resource.DoesNotExist:
+            graph = models.GraphModel.objects.get(pk=resourceid)
+            resourceid = None
+            resource_instance = None
+            pass
+
+        nodegroups = []
+        editable_nodegroups = []
+        nodes = graph.node_set.all().select_related("nodegroup")
+        for node in nodes:
+            if node.is_collector:
+                added = False
+                if request.user.has_perm("write_nodegroup", node.nodegroup):
+                    editable_nodegroups.append(node.nodegroup)
+                    nodegroups.append(node.nodegroup)
+                    added = True
+                if not added and request.user.has_perm(
+                    "read_nodegroup", node.nodegroup
+                ):
+                    nodegroups.append(node.nodegroup)
+
+        user_is_reviewer = user_is_resource_reviewer(request.user)
+
+        if resource_instance is None:
+            tiles = []
+            displayname = _("New Resource")
+        else:
+            displayname = resource_instance.displayname()
+            if displayname == "undefined":
+                displayname = _("Unnamed Resource")
+            if (
+                str(resource_instance.graph_id)
+                == settings.SYSTEM_SETTINGS_RESOURCE_MODEL_ID
+            ):
+                displayname = _("System Settings")
+
+            tiles = resource_instance.tilemodel_set.order_by("sortorder").filter(
+                nodegroup__in=nodegroups
+            )
+            provisionaltiles = []
+            for tile in tiles:
+                append_tile = True
+                isfullyprovisional = False
+                if tile.provisionaledits is not None:
+                    if len(list(tile.provisionaledits.keys())) > 0:
+                        if len(tile.data) == 0:
+                            isfullyprovisional = True
+                        if user_is_reviewer is False:
+                            if str(request.user.id) in tile.provisionaledits:
+                                tile.provisionaledits = {
+                                    str(request.user.id): tile.provisionaledits[
+                                        str(request.user.id)
+                                    ]
+                                }
+                                tile.data = tile.provisionaledits[str(request.user.id)][
+                                    "value"
+                                ]
+                            else:
+                                if isfullyprovisional is True:
+                                    # if the tile IS fully provisional and the current user is not the owner,
+                                    # we don't send that tile back to the client.
+                                    append_tile = False
+                                else:
+                                    # if the tile has authoritaive data and the current user is not the owner,
+                                    # we don't send the provisional data of other users back to the client.
+                                    tile.provisionaledits = None
+                if append_tile is True:
+                    provisionaltiles.append(tile)
+            tiles = provisionaltiles
+
+        serialized_graph = None
+        if graph.publication:
+            user_language = translation.get_language()
+            published_graph = models.PublishedGraph.objects.get(
+                publication=graph.publication, language=user_language
+            )
+            serialized_graph = published_graph.serialized_graph
+
+        if serialized_graph:
+            serialized_cards = serialized_graph["cards"]
+            cardwidgets = [
+                widget
+                for widget in models.CardXNodeXWidget.objects.filter(
+                    pk__in=[
+                        widget_dict["id"] for widget_dict in serialized_graph["widgets"]
+                    ]
+                )
+            ]
+        else:
+            cards = (
+                graph.cardmodel_set.order_by("sortorder")
+                .filter(nodegroup__in=nodegroups)
+                .prefetch_related("cardxnodexwidget_set")
+            )
+            serialized_cards = JSONSerializer().serializeToPython(cards)
+            cardwidgets = [
+                widget
+                for widget in [
+                    card.cardxnodexwidget_set.order_by("sortorder").all()
+                    for card in cards
+                ]
+            ]
+
+        editable_nodegroup_ids = [
+            str(nodegroup.pk) for nodegroup in editable_nodegroups
+        ]
+        for card in serialized_cards:
+            card["is_writable"] = False
+            if str(card["nodegroup_id"]) in editable_nodegroup_ids:
+                card["is_writable"] = True
+
+        my_tiles = []
+        for card in serialized_cards:
+            if card["name"] == "Location Data":
+                my_tiles.append(
+                    {
+                        "data": {},
+                        "nodegroup_id": card["nodegroup_id"],
+                        "parenttile_id": None,
+                        "provisionaledits": None,
+                        "resourceinstance_id": uuid.uuid4(),
+                        "sortorder": 0,
+                        "tileid": "dummy-id",
+                    }
+                )
+
+        context = {
+            "resourceid": resourceid,
+            "displayname": displayname,
+            "tiles": tiles + my_tiles,
+            "cards": serialized_cards,
+            "nodegroups": nodegroups,
+            "nodes": nodes.filter(nodegroup__in=nodegroups),
+            "cardwidgets": cardwidgets,
+            "datatypes": models.DDataType.objects.all(),
+            "userisreviewer": user_is_reviewer,
+            "widgets": models.Widget.objects.all(),
+            "card_components": models.CardComponent.objects.all(),
+        }
+
+        return JSONResponse(context, indent=4)
