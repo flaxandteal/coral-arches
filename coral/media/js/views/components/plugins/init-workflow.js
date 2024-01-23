344ce5756025812ec97ca19efbae3fd76fa8a6c4
Taylor North <taylor@galviadigital.com>
2024-01-23 17:00:09 +0000

    Migrated init workflow and added flag enforcement to the list

---
+++ coral/media/js/views/components/plugins/init-workflow.js
@@ -0,0 +1,18 @@
+define([
+  'knockout',
+  'arches',
+  'templates/views/components/plugins/init-workflow.htm'
+], function(ko, arches, initWorkflowTemplate) {
+
+  var InitWorkflow = function(params) {
+      this.workflows = params.workflows.map(function(wf){
+          wf.url = arches.urls.plugin(wf.slug);
+          return wf;
+      }, this);
+  };
+
+  return ko.components.register('init-workflow', {
+      viewModel: InitWorkflow,
+      template: initWorkflowTemplate
+  });
+});
