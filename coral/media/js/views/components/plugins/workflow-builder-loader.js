453c40f9f55614b4560d6cc69c2fae61f60e93d4
teenorth <teenorth@protonmail.com>
2024-01-24 21:15:25 +0000

    Prettier formatting

--- coral/media/js/views/components/plugins/workflow-builder-loader.js
+++ coral/media/js/views/components/plugins/workflow-builder-loader.js
@@ -4,7 +4,7 @@ define([
   'viewmodels/editable-workflow',
   'templates/views/components/plugins/default-workflow.htm',
   'views/components/workflows/default-card-util',
-  'views/components/workflows/workflow-builder-initial-step',
+  'views/components/workflows/workflow-builder-initial-step'
 ], function (ko, arches, EditableWorkflow, workflowTemplate) {
   return ko.components.register('workflow-builder-loader', {
     viewModel: function (params) {
