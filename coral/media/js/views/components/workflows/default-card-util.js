8e73ddb8d841248b331ed786a374d4ed60d440ed
Taylor North <taylor@galviadigital.com>
2024-02-08 14:30:39 +0000

    Default card now includes the fix for saving many tiles

--- coral/media/js/views/components/workflows/default-card-util.js
+++ coral/media/js/views/components/workflows/default-card-util.js
@@ -5,8 +5,9 @@ define([
   'uuid',
   'arches',
   'viewmodels/card-component',
+  'viewmodels/alert',
   'templates/views/components/cards/default.htm'
-], function (_, ko, koMapping, uuid, arches, CardComponentViewModel, widgetLabeller) {
+], function (_, ko, koMapping, uuid, arches, CardComponentViewModel, AlertViewModel, widgetLabeller) {
   function viewModel(params) {
     CardComponentViewModel.apply(this, [params]);
 
@@ -34,6 +35,102 @@ define([
           }
         });
       });
+
+    /**
+     * Overridden function saveMultiTiles from WorkflowComponentAbstract.
+     *
+     * Fixes:
+     *   - Saving when no tiles are added
+     *   - Clearing local storage after removing a tile
+     *     so the tile won't exist when the user
+     *     returns to that step
+     */
+    this.form.saveMultiTiles = async () => {
+      this.form.complete(false);
+      this.form.saving(true);
+      this.previouslyPersistedComponentData = [];
+
+      /**
+       * Original version of this method that has been
+       * overridden here didn't end the save if there
+       * wasn't any tiles saved. Multi tile steps can
+       * now progress if no data was provided.
+       */
+      if (this.tiles().length === 0 && this.tilesToRemove().length === 0) {
+        this.form.complete(true);
+        this.form.loading(true);
+        this.form.saving(false);
+
+        return;
+      }
+
+      const unorderedSavedData = ko.observableArray();
+
+      this.form.tiles().forEach((tile) => {
+        tile.save(
+          () => {
+            /* onFail */
+          },
+          (savedTileData) => {
+            unorderedSavedData.push(savedTileData);
+          }
+        );
+      });
+
+      this.form.tilesToRemove().forEach((tile) => {
+        tile.deleteTile(
+          (response) => {
+            this.form.alert(
+              new AlertViewModel(
+                'ep-alert-red',
+                response.responseJSON.title,
+                response.responseJSON.message,
+                null,
+                () => {
+                  return;
+                }
+              )
+            );
+          },
+          () => {
+            this.form.tilesToRemove.remove(tile);
+            /**
+             * This functionality wasn't needed
+             */
+            // if (this.tilesToRemove().length === 0) {
+            //   //   this.complete(true);
+            //   //   this.loading(true);
+            //   //   this.saving(false);
+            // }
+          }
+        );
+      });
+
+      if (!this.form.tiles().length) {
+        this.form.complete(true);
+        this.form.loading(true);
+        this.form.saving(false);
+        this.form.savedData([]);
+      }
+
+      const saveSubscription = unorderedSavedData.subscribe((savedData) => {
+        if (savedData.length === this.form.tiles().length) {
+          this.form.complete(true);
+          this.form.loading(true);
+          this.form.saving(false);
+
+          const orderedSavedData = this.form.tiles().map((tile) => {
+            return savedData.find((datum) => {
+              return datum.tileid === tile.tileid;
+            });
+          });
+          
+          this.form.savedData(orderedSavedData.reverse());
+
+          saveSubscription.dispose(); /* this-disposing subscription only runs once */
+        }
+      });
+    };
   }
 
   ko.components.register('default-card-util', {
