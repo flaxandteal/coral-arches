7692741040caf58155661c005494563afddc6a8f
Kenny Gozali <Kenny.gozali@flaxandteal.co.uk>
2024-02-13 11:14:58 +0000

    fix: added localhost:8000 into the cy.visit()

--- cypress/e2e/sample_spec.js
+++ cypress/e2e/sample_spec.js
@@ -1,12 +1,12 @@
 describe('Home page', function() {
   it('Visits the home page', function() {
-    cy.visit('/')
+    cy.visit('localhost:8000/')
   })
 })
 
 describe('Log in as admin', function() {
   it('Goes to login page, logs in with default creds, and should redirect to index', function() {
-    cy.visit('/auth/?next=/index.htm')
+    cy.visit('localhost:8000/auth/?next=/index.htm')
 
     cy.get('.input-group > .floating-label-group > input[name="username"].form-control').type(`admin{enter}`)
     cy.get('.input-group > .floating-label-group > input[name="password"].form-control').type(`admin{enter}`)
