ded672e8e367d1be09562a6bdce65bc3ca74c147
Taylor North <taylor@galviadigital.com>
2024-02-16 15:09:56 +0000

    Allowed select resource to also find tiles and their ids

--- coral/media/js/views/components/workflows/select-resource-id.js
+++ coral/media/js/views/components/workflows/select-resource-id.js
@@ -11,24 +11,51 @@ define([
   function viewModel(params) {
     const ResourceInstanceSelectViewModel = require('viewmodels/resource-instance-select');
 
-    params.multiple = false;
-    params.datatype = 'resource-instance';
     params.configKeys = ['placeholder', 'defaultResourceInstance'];
     params.graphids = params.graphIds;
 
     ResourceInstanceSelectViewModel.apply(this, [params]);
 
+    this.tileIds = ko.observable(params.form.savedData()?.tileIds || {});
+
     if (this.form.savedData()?.selectedResourceId) {
       this.value(this.form.savedData()?.selectedResourceId);
     }
 
-    this.form.save = () => {
+    if (params.label) {
+      this.label(params.label);
+    }
+
+    this.fetchTileData = async (resourceId, nodeId) => {
+      const tilesResponse = await window.fetch(
+        arches.urls.resource_tiles.replace('aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', resourceId) +
+          (nodeId ? `?nodeid=${nodeId}` : '')
+      );
+      const data = await tilesResponse.json();
+      return data.tiles;
+    };
+
+    this.getTile = async ({ nodegroupId, lookupName }) => {
+      const tiles = await this.fetchTileData(this.value(), nodegroupId);
+
+      if (!tiles.length === 1) return;
+
+      this.tileIds()[lookupName] = tiles[0].tileid;
+    };
+
+    this.form.save = async () => {
       if (!this.value()) {
         params.form.saving(false);
         return;
-      };
+      }
+      await Promise.all(params.getTileIdFromNodegroup.map(this.getTile));
+
+      // TODO: Catch errors
+
       this.form.savedData({
-        selectedResourceId: this.value()
+        selectedResourceId: this.value(),
+        tileIds: this.tileIds(),
+        ...this.tileIds()
       });
       params.form.complete(true);
       params.form.saving(false);
