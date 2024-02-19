c37ed82472844d9106857367429e5a1e58f6995b
Kenny Gozali <Kenny.gozali@flaxandteal.co.uk>
2024-02-19 11:00:52 +0000

    wip: archive source workflow testing

--- cypress/e2e/archivesource.cy.js
+++ cypress/e2e/archivesource.cy.js
@@ -1,7 +1,15 @@
-describe('Opens the Workflow', function() {
-    it('Shows the workflow page', function() {
+describe('Working With The Archive Source Workflow', function() {
+    it('Makes an Archive Source', function() {
         cy.login();
-        cy.visit("http://localhost:8000/plugins/init-workflow")
-        cy.contains("Workflows")
+        cy.visit("http://localhost:8000/plugins/init-workflow");
+        cy.contains("Workflows");
+        cy.contains("A workflow for Archive Sources and Cataloguing").click();
+        cy.contains("Start New").click();
+        // Starting the workflow
+        // Initial Step
+        cy.contains("Save and Continue").click()
+        // Initial Step
+        // cy.contains("Next Step").click();
+        // cy.get('[aria-label="Archive Source Name"]').click().type('Hello World');
     })
 })
