979ffea3fde9014ce12f854f1a014de2af16bf36
Phil Weir <phil.weir@flaxandteal.co.uk>
2024-02-24 18:54:15 +0000

    add non-dummy cache to allow building of production pages

--- coral/settings.py
+++ coral/settings.py
@@ -369,7 +369,8 @@ SESSION_COOKIE_NAME = 'coral'
 # For more info on configuring your cache: https://docs.djangoproject.com/en/2.2/topics/cache/
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+        "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+        "LOCATION": "unique-snowflake"
     },
     'user_permission': {
         'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
