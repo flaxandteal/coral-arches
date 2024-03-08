dec5e51b2d3329bc4b615990d1ddc712c2720a31
Taylor North <taylor@galviadigital.com>
2024-03-08 16:54:36 +0000

    version: v2.29.22

--- coral/settings.py
+++ coral/settings.py
@@ -17,7 +17,7 @@ except ImportError:
     pass
 
 APP_NAME = 'coral'
-APP_VERSION = semantic_version.Version(major=2, minor=28, patch=22)
+APP_VERSION = semantic_version.Version(major=2, minor=29, patch=22)
 
 GROUPINGS = {
     "groups": {
