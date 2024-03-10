742872c8af703eefb1ba8122201b46ea17b3a056
Phil Weir <phil.weir@flaxandteal.co.uk>
2024-03-10 04:20:39 +0100

    feat(remapping): add in support for remapping API objects

--- coral/settings.py
+++ coral/settings.py
@@ -2,6 +2,11 @@
 Django settings for coral project.
 """
 
+try:
+    import tomllib
+except ImportError:
+    from pip._vendor import tomli as tomllib
+
 import json
 import os
 import sys
@@ -83,134 +88,9 @@ TEMPLATES[0]['DIRS'].insert(0, os.path.join(APP_ROOT, 'templates'))
 
 ANONYMOUS_SETS = []
 
-WELL_KNOWN_RESOURCE_MODELS = [
-    dict(
-        model_name="Person",
-        graphid="22477f01-1a44-11e9-b0a9-000d3ab1e588",
-        user_account={
-            "type": "user",
-            "lang": "en",
-            "nodegroupid": "b1f5c336-6a0e-11ee-b748-0242ac140009",
-            "nodeid": "b1f5c336-6a0e-11ee-b748-0242ac140009",
-        },
-    ),
-    {
-        "model_name": "Group",
-        "graphid": "07883c9e-b25c-11e9-975a-a4d18cec433a",
-        "permissions/object": {
-            "type": "@Set",
-            "lang": "en",
-            "nodegroupid": "ae2039a4-7070-11ee-bb7a-0242ac140008",
-            "nodeid": "448bcdb8-7071-11ee-8b8c-0242ac140008",
-        },
-        "permissions/action": {
-            "type": "concept-list",
-            "lang": "en",
-            "nodegroupid": "ae2039a4-7070-11ee-bb7a-0242ac140008",
-            "nodeid": "7cb692b2-7072-11ee-bb7a-0242ac140008",
-        }
-    },
-    dict(
-        model_name="Set",
-        graphid="b16832e8-dfc9-4fc8-9c07-0c0b980ed220",
-    ),
-    dict(
-        model_name="Logical Set",
-        graphid="5b8b4084-9687-11ee-8782-0242ac140006",
-    ),
-    dict(
-        model_name="Organization",
-        graphid="d4a88461-5463-11e9-90d9-000d3ab1e588",
-    ),
-    dict(
-        model_name="Activity",
-        graphid="b9e0701e-5463-11e9-b5f5-000d3ab1e588"
-    ),
-    dict(
-        model_name="Application Area",
-        graphid="42ce82f6-83bf-11ea-b1e8-f875a44e0e11"
-    ),
-    dict(
-        model_name="Archive Source",
-        graphid="b07cfa6f-894d-11ea-82aa-f875a44e0e11"
-    ),
-    dict(
-        model_name="Area",
-        graphid="979aaf0b-7042-11ea-9674-287fcf6a5e72"
-    ),
-    dict(
-        model_name="Artefact",
-        graphid="343cc20c-2c5a-11e8-90fa-0242ac120005"
-    ),
-    dict(
-        model_name="Bibliographic Source",
-        graphid="24d7b54f-5464-11e9-a86b-000d3ab1e588"
-    ),
-    dict(
-        model_name="Consultation Clone",
-        graphid="8effdca4-ffb6-482b-94b8-4d35fb5c88c5"
-    ),
-    dict(
-        model_name="Consultation",
-        graphid="8d41e49e-a250-11e9-9eab-00224800b26d"
-    ),
-    dict(
-        model_name="Digital Object",
-        graphid="a535a235-8481-11ea-a6b9-f875a44e0e11"
-    ),
-    dict(
-        model_name="Enforcement",
-        graphid="8c3a4ae7-2704-4f47-aa68-4da7f9fc6d84"
-    ),
-    dict(
-        model_name="Grant",
-        graphid="fa47c72c-ec07-11ed-b9ad-72d420f37f11"
-    ),
-    dict(
-        model_name="Group",
-        graphid="07883c9e-b25c-11e9-975a-a4d18cec433a"
-    ),
-    dict(
-        model_name="Heritage Story",
-        graphid="0add0e11-99aa-11ea-9ab8-f875a44e0e11"
-    ),
-    dict(
-        model_name="Historic Aircraft",
-        graphid="b8032b00-594d-11e9-9cf0-18cf5eb368c4"
-    ),
-    dict(
-        model_name="Historic Landscape Characterization",
-        graphid="934cd7f0-480a-11ea-9240-c4d9877d154e"
-    ),
-    dict(
-        model_name="License",
-        graphid="cc5da227-24e7-4088-bb83-a564c4331efd"
-    ),
-    dict(
-        model_name="Maritime Vessel",
-        graphid="49bac32e-5464-11e9-a6e2-000d3ab1e588"
-    ),
-    dict(
-        model_name="Merge Resource Tracker",
-        graphid="d9318eb6-f28d-427c-b061-6fe3021ce8aa"
-    ),
-    dict(
-        model_name="Monument",
-        graphid="076f9381-7b00-11e9-8d6b-80000b44d1d9"
-    ),
-    dict(
-        model_name="Monument Revision",
-        graphid="4262df46-eabf-11ed-9e22-72d420f37f11"
-    ),
-    dict(
-        model_name="Period",
-        graphid="f9045867-8861-11ea-b06f-f875a44e0e11"
-    ),
-    dict(
-        model_name="Place",
-        graphid="78b32d8c-b6f2-11ea-af42-f875a44e0e11"
-    )
-]
+with (Path(__file__).parent / "wkrm.toml").open("rb") as wkrm_f:
+    WELL_KNOWN_RESOURCE_MODELS = [model for _, model in tomllib.load(wkrm_f).items()]
+print(WELL_KNOWN_RESOURCE_MODELS)
 
 LOCALE_PATHS.append(os.path.join(APP_ROOT, 'locale'))
 
