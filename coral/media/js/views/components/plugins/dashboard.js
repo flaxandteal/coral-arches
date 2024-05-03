15475685ea93e4f473a58c9bb571b117cd9ebb7d
Stuart Marshall <stuart.marshall@flaxandteal.co.uk>
2024-05-03 11:55:42 +0100

    add task counters with mock data

--- coral/media/js/views/components/plugins/dashboard.js
+++ coral/media/js/views/components/plugins/dashboard.js
@@ -9,9 +9,59 @@ define([
   ], function ($, ko, _, koMapping, arches, pageTemplate) {
 
     const pageViewModel = function (params) {
+
+        this.counters = ko.observableArray([
+            { label: 'Total', count: ko.observable(0)},
+            { label: 'New', count: ko.observable(0)},
+            { label: 'In Progress', count: ko.observable(0)},
+        ]);
+
+        this.resources = ko.observableArray([
+            {
+                displayName: 'Enforcement',
+                description: 'This is a description',
+                status: 'In Progress',
+                resourceId: 'resourceId1'
+            },
+            {
+                displayName: 'Excavation',
+                description: 'This is a description',
+                status: 'In Progress',
+                resourceId: 'resourceId2'
+            },
+            {
+                displayName: 'Monument',
+                description: 'This is a description',
+                status: 'New',
+                resourceId: 'resourceId3'
+            },
+            {
+                displayName: 'License',
+                description: 'This is a description',
+                status: 'New',
+                resourceId: 'resourceId4'
+            },
+        ])
+
+        this.updateCounters = function() {
+            this.counters().forEach(element => {
+                switch(element.label) {
+                    case 'Total':
+                        element.count(this.resources().length);
+                        break;
+                    case 'New':
+                        element.count(this.resources().filter(resource => resource.status === 'New').length);
+                        break;
+                    case 'In Progress':
+                        element.count = (this.resources().filter(resource => resource.status === 'In Progress').length);
+                        break;
+                }
+            });
+        }
+
+        this.updateCounters();
     }
 
-  
     return ko.components.register('dashboard', {
       viewModel: pageViewModel,
       template: pageTemplate
