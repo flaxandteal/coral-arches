3aaf85cc334d3961ddf2606516db78e38468de2b
Taylor North <taylor@galviadigital.com>
2024-05-07 10:56:31 +0100

    feat: add new calculate composite score to workflow

--- coral/media/js/views/components/plugins/workflow-builder-loader.js
+++ coral/media/js/views/components/plugins/workflow-builder-loader.js
@@ -13,7 +13,8 @@ define([
   'views/components/workflows/related-document-upload',
   'views/components/workflows/file-template',
   'views/components/workflows/fmw-workflow/get-selected-monument-details',
-  'views/components/workflows/excavation-site-visit-workflow/get-selected-license-details'
+  'views/components/workflows/excavation-site-visit-workflow/get-selected-license-details',
+  'views/components/workflows/fmw-workflow/calculate-composite-score',
 ], function (ko, arches, OpenableWorkflow, workflowTemplate) {
   return ko.components.register('workflow-builder-loader', {
     viewModel: function (params) {
