5793e3ab3ea785d26fcab7007749498fa4a1bd6d
Stuart Marshall <stuart.marshall@flaxandteal.co.uk>
2024-05-13 09:30:24 +0100

    refactor groups to use arches orm

--- coral/views/dashboard.py
+++ coral/views/dashboard.py
@@ -31,20 +31,15 @@ class Dashboard(View):
             return JsonResponse({"error": "User not found"}, status=404)
         
         personId = str(person_resource[0].id)
+        
+        groups = Group.all()
 
-        members = Tile.objects.filter(nodegroup_id = MEMBERS_NODEGROUP)
-
-        #this searches tiles for groups that include the user
         userGroupIds = []
-        for tile in members:
-            object_data = tile.data.get(MEMBERS_NODEGROUP, {})
 
-            if object_data is None:
-                continue
-
-            for object in object_data:
-                if 'resourceId' in object and object['resourceId'] == personId:
-                    userGroupIds.append(str(tile.resourceinstance_id))
+        for group in groups:
+            for member in group.members:
+                if member.id == person_resource[0].id:
+                    userGroupIds.append(str(group.id)) #needs to be a string and not uuid
         
         taskResources = []
 
@@ -56,8 +51,6 @@ class Dashboard(View):
         return JsonResponse(taskResources, safe=False)
 
     def get_planning_consultations(self, userGroupIds):
-        ACTION_STATUS_NODE = '19eef70c-69b8-11ee-8431-0242ac120002'
-        ACTION_TYPE_NODE = 'e2585f8a-51a3-11eb-a7be-f875a44e0e11'
 
         TYPE_ASSIGN_HM = '94817212-3888-4b5c-90ad-a35ebd2445d5'
         TYPE_ASSIGN_HB = '12041c21-6f30-4772-b3dc-9a9a745a7a3f'
@@ -66,40 +59,28 @@ class Dashboard(View):
         HM_GROUP = '905c40e1-430b-4ced-94b8-0cbdab04bc33'
         HB_GROUP = '9a88b67b-cb12-4137-a100-01a977335298'
 
-        ActionTiles = Tile.objects.filter(nodegroup_id = ACTION_NODEGROUP) 
+        is_hm_group = HM_GROUP in userGroupIds
+        is_hb_group = HB_GROUP in userGroupIds
 
-        resources = []        
+        resources = [] 
 
-        #get resource id for all consultations status not closed
-        for tile in ActionTiles:
-            status_node = tile.data.get(ACTION_STATUS_NODE, {})
-            action_type = tile.data.get(ACTION_TYPE_NODE, {})
-            
-            if status_node == STATUS_CLOSED:
-                continue
+        consultations = Consultation.all()
 
-            is_hm_group = HM_GROUP in userGroupIds
-            is_hb_group = HB_GROUP in userGroupIds
+        #filter out consultations that are not planning consultations
+        planning_consultations=[c for c in consultations if c._name().startswith('CON/')]
+        for consultation in planning_consultations:
+                action_status = consultation.action[0].action_status if consultation.action else None
+                action_type = consultation.action[0].action_type if consultation.action else None
 
-            conditions_for_task = (
-                (is_hm_group and status_node == STATUS_OPEN and action_type in [TYPE_ASSIGN_HM, TYPE_ASSIGN_BOTH]) or
-                (is_hb_group and status_node == STATUS_OPEN and action_type in [TYPE_ASSIGN_HB, TYPE_ASSIGN_BOTH]) or
-                (not is_hm_group and not is_hb_group)
-            )
-
-            if conditions_for_task:
-                task = self.build_planning_resource_data(tile.resourceinstance_id, status_node)
-                resources.append(task)
-
-        #get consultations with missing action tile
-        if not is_hm_group and not is_hb_group:
-            consultations= Consultation.all()
-            for consultation in consultations:
-                if consultation is not None and not consultation.action:
-                    task = self.build_planning_resource_data(consultation.id, 'None')
+                conditions_for_task = (
+                    (is_hm_group and action_status == STATUS_OPEN and action_type in [TYPE_ASSIGN_HM, TYPE_ASSIGN_BOTH]) or
+                    (is_hb_group and action_status == STATUS_OPEN and action_type in [TYPE_ASSIGN_HB, TYPE_ASSIGN_BOTH]) or
+                    (not is_hm_group and not is_hb_group)
+                )
+                if conditions_for_task:                 
+                    task = self.build_planning_resource_data(consultation)
                     if task:
                         resources.append(task)
-
         return resources
     
     def convert_id_to_string(self, id):
@@ -120,19 +101,14 @@ class Dashboard(View):
         elif id == None:
             return 'None'
     
-    def build_planning_resource_data(self, resourceId, status):
-        resource = Resource.objects.get(resourceinstanceid = resourceId)
-        display_name = resource.displayname()
-
-        if not display_name.startswith('CON/'):
-            return
-        hierarchy_type_tile = Tile.objects.get(nodegroup_id = HIERARCHY_NODE_GROUP)
-        hierarchy_type = hierarchy_type_tile.data.get(HIERARCHY_NODE_GROUP, {})
+    def build_planning_resource_data(self, consultation):
+        action_status = consultation.action[0].action_status if consultation.action else None
+        hierarchy_type = consultation.hierarchy_type
         resource_data = {
-            'id': str(resourceId),
-            'displayname': display_name,
-            'displaydescription': resource.displaydescription(),
-            'status': self.convert_id_to_string(status),
+            'id': consultation.id,
+            'displayname': consultation._name(),
+            'displaydescription': consultation._description(),
+            'status': self.convert_id_to_string(action_status),
             'hierarchy_type': self.convert_id_to_string(hierarchy_type),
             'responseslug': 'assign-consultation-workflow'
         }
