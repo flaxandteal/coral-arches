99f3eea870eb34337b6d798d894a4add53f9cafb
Taylor North <taylor@galviadigital.com>
2024-07-10 16:15:17 +0100

    feat: frontend component to generate smr number and save it into the correct fields

--- coral/media/js/views/components/workflows/generate-smr-number.js
+++ coral/media/js/views/components/workflows/generate-smr-number.js
@@ -13,22 +13,23 @@ define([
 
     this.smrNumber = ko.observable('');
 
-    this.NISMR_NUMBERING_TYPE = '86c19e92-3ea7-11ef-818b-0242ac140006';
+    this.NISMR_NUMBERING_TYPE_NODE_ID = '86c19e92-3ea7-11ef-818b-0242ac140006';
+    this.GENERATED_SMR_NODE_ID = 'b46b5bba-3ec2-11ef-bb61-0242ac140006';
 
-    this.tile.data[this.NISMR_NUMBERING_TYPE].subscribe((value) => {
+    this.tile.data[this.NISMR_NUMBERING_TYPE_NODE_ID].subscribe((value) => {
       console.log('nismr number: ', value);
     }, this);
 
     this.hasSelectedNismr = ko.computed(() => {
-      return !!this.tile.data[this.NISMR_NUMBERING_TYPE]();
+      return !!this.tile.data[this.NISMR_NUMBERING_TYPE_NODE_ID]();
     }, this);
 
     this.generateSmrNumber = async () => {
-      if (!this.tile.data[this.NISMR_NUMBERING_TYPE]()) return;
+      if (!this.tile.data[this.NISMR_NUMBERING_TYPE_NODE_ID]()) return;
       params.pageVm.loading(true);
       const data = {
         resourceInstanceId: this.tile.resourceinstance_id,
-        selectedNismrId: this.tile.data[this.NISMR_NUMBERING_TYPE]()
+        selectedNismrId: this.tile.data[this.NISMR_NUMBERING_TYPE_NODE_ID]()
       };
       const response = await $.ajax({
         type: 'POST',
@@ -40,7 +41,19 @@ define([
           console.log(response, status, error);
         }
       });
-      console.log('response: ', response);
+      if (ko.isObservable(this.tile.data[this.GENERATED_SMR_NODE_ID])) {
+        this.tile.data[this.GENERATED_SMR_NODE_ID]({
+          en: {
+            value: response.smrNumber
+          }
+        });
+      } else {
+        this.tile.data[this.GENERATED_SMR_NODE_ID] = {
+          en: {
+            value: response.smrNumber
+          }
+        };
+      }
       params.pageVm.loading(false);
     };
   }
