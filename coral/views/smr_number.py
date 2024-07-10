f6a4723e8a95f117a5e1e92146af27525bc6cfd0
Taylor North <taylor@galviadigital.com>
2024-07-10 16:33:51 +0100

    feat: working lifecycle for generating the smr number with one exception that non-localised-string was used for the refs needing changes in utils/smr_number.py

--- coral/views/smr_number.py
+++ coral/views/smr_number.py
@@ -6,8 +6,8 @@ from coral.utils.smr_number import SmrNumber
 from arches.app.models import models
 
 
-SYSTEM_REFERENCE_NODEGROUP = "325a2f2f-efe4-11eb-9b0c-a87eeabdefba"
-SYSTEM_REFERENCE_RESOURCE_ID_NODE_ID = "325a430a-efe4-11eb-810b-a87eeabdefba"
+HERITAGE_ASSET_REFERENCES_NODEGROUP_ID = "e71df5cc-3aad-11ef-a2d0-0242ac120003"
+SMR_NUMBER_NODE_ID = "158e1ed2-3aae-11ef-a2d0-0242ac120003"
 
 
 class SmrNumberView(View):
@@ -16,37 +16,29 @@ class SmrNumberView(View):
         resource_instance_id = data.get("resourceInstanceId")
         selected_nismr_id = data.get("selectedNismrId")
 
-        print("resource_instance_id: ", resource_instance_id)
-        print("selected_nismr_id: ", selected_nismr_id)
+        map_sheet_id = models.Value.objects.filter(valueid=selected_nismr_id).first()
 
-        value = models.Value.objects.filter(valueid=selected_nismr_id).first()
+        if resource_instance_id:
+            references_tile = Tile.objects.filter(
+                resourceinstance_id=resource_instance_id,
+                nodegroup_id=HERITAGE_ASSET_REFERENCES_NODEGROUP_ID,
+            ).first()
 
-        print('value: ', value)
-        print('value.value: ', value.value)
+            if references_tile and references_tile.data.get(SMR_NUMBER_NODE_ID, None):
+                id = (
+                    references_tile.data.get(SMR_NUMBER_NODE_ID, None)
+                    .get("en")
+                    .get("value")
+                )
+                print("SMR Number has already been generated: ", id)
+                return JSONResponse(
+                    {
+                        "message": "SMR Number has already been generated",
+                        "haNumber": id,
+                    }
+                )
 
-        # if resource_instance_id:
-        #     sys_ref_tile = Tile.objects.filter(
-        #         resourceinstance_id=resource_instance_id,
-        #         nodegroup_id=SYSTEM_REFERENCE_NODEGROUP,
-        #     ).first()
+        sn = SmrNumber(map_sheet_id=map_sheet_id.value)
+        smr_number = sn.generate_id_number(resource_instance_id)
 
-        #     if sys_ref_tile and sys_ref_tile.data.get(
-        #         SYSTEM_REFERENCE_RESOURCE_ID_NODE_ID, None
-        #     ):
-        #         id = (
-        #             sys_ref_tile.data.get(SYSTEM_REFERENCE_RESOURCE_ID_NODE_ID, None)
-        #             .get("en")
-        #             .get("value")
-        #         )
-        #         print("Heritage Asset ID has already been generated: ", id)
-        #         return JSONResponse(
-        #             {
-        #                 "message": "Heritage Asset ID has already been generated",
-        #                 "haNumber": id,
-        #             }
-        #         )
-
-        # sn = SmrNumber()
-        # smr_number = sn.generate_id_number(resource_instance_id)
-
-        return JSONResponse({"message": "Generated ID", "smrNumber": None})
+        return JSONResponse({"message": "Generated ID", "smrNumber": smr_number})
