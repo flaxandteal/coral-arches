e2c9501a1292eab6963b944271b4ba954f2f8368
Taylor North <135817240+taylorn01@users.noreply.github.com>
2024-07-30 11:25:02 +0100

    fix: tile save errors will now produce an error

--- coral/media/js/views/components/workflows/workflow-component-abstract.js
+++ coral/media/js/views/components/workflows/workflow-component-abstract.js
@@ -584,7 +584,16 @@ define([
 
             self.tiles().forEach(function(tile) {
                 tile.save(
-                    function(){/* onFail */}, 
+                    function(response){
+                        self.alert(new AlertViewModel(
+                            'ep-alert-red', 
+                            response.responseJSON.title,
+                            response.responseJSON.message,
+                            null, 
+                            function(){ return; }
+                        ));
+                        self.saving(false);
+                    }, 
                     function(savedTileData) {
                         unorderedSavedData.push(savedTileData);
                     }
