7dbf1d8ad77a6e5428c9d54771bec92328708577
Taylor North <135817240+taylorn01@users.noreply.github.com>
2024-07-31 09:43:07 +0100

    version: v4.19.1

--- coral/settings.py
+++ coral/settings.py
@@ -22,7 +22,7 @@ except ImportError:
     pass
 
 APP_NAME = 'coral'
-APP_VERSION = semantic_version.Version(major=4, minor=19, patch=0)
+APP_VERSION = semantic_version.Version(major=4, minor=19, patch=1)
 
 GROUPINGS = {
     "groups": {
