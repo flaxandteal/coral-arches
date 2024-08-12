c3ecd8fa3b79127e7b49d7c253800a86373bda3a
Andrea K <andrea.kyurchiev@flaxandteal.co.uk>
2024-08-12 14:43:44 +0300

    wip: feat: selects first option in dropdown

--- cypress/e2e/flag_for_enforcement_workflow.cy.js
+++ cypress/e2e/flag_for_enforcement_workflow.cy.js
@@ -10,17 +10,10 @@ describe('Going through the Flag For Enforcement Workflow', function () {
         cy.contains("Flag for Enforcement").click();
         // Starting the workflow
         cy.contains("Save and Continue").click();
-        cy.wait(5000);
-        cy.get('[aria-label="Case Reference"]').should('be.visible').click().type('Case Ref');
-        // cy.get('#cke_2_contents > .cke_wysiwyg_frame').click().type('test');
-        // cy.wait(5000);
-        // cy.get('[aria-label="Subtitle"]').should('be.visible').click().type('Subtitle');
-        // cy.get('[aria-label="File ID (key)"]').should('be.visible').click().type('File ID (key)');
-        // cy.get('#cke_2_contents').click().type('test type ref');
-        cy.contains("Save and Continue").click();
-        // cy.get('[aria-label="Flagged by"]').should('be.visible').click().type('Case Ref');
-        // cy.get('.card_component.full_name > .row > .form-group > [style="max-width: 600px; position: relative"] > .col-xs-12 > .form-control').type('Test');
-        // cy.get(':nth-child(2) > .card-component > .new-provisional-edit-card-container > .card > .install-buttons > .fa-plus > span').click();
-        // cy.get('.close-new-step > .btn > span').click();
+        cy.wait(2000);
+        cy.get('[aria-label="Case Reference"]').should('be.visible').type('Case Ref');
+        cy.contains("Flagged by").siblings('.row').click();
+        // Selects the first value in the dropdown. Would be better if we have a pervious test that initates a test person and we can find them by name
+        cy.get('.select2-results__options li').first().click();
     })
 })
