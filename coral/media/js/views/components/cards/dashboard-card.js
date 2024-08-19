2158024ed9159a32c4a9e930f5cc70690b2527ab
Stuart Marshall <stuart.marshall@flaxandteal.co.uk>
2024-08-19 16:37:10 +0100

    refactor(2295): Make dashboard card a separate component

---
+++ coral/media/js/views/components/cards/dashboard-card.js
@@ -0,0 +1,27 @@
+define([
+    'knockout',
+    'underscore',
+    'knockout-mapping',
+    'arches',
+    'templates/views/components/cards/dashboard-card.htm',
+  ], function (ko, _, koMapping, arches, dashboardCardTemplate,) {
+    function DashboardCardViewModel(params) {
+        self = this
+        self.data = params.data
+        self.state = params.data.state
+        console.log(self.data.displaydescription)
+
+        this.openFlagged = (resourceId, responseSlug) => {
+                localStorage.setItem('workflow-open-mode', JSON.stringify(true));
+                let url = arches.urls.plugin(
+                `${responseSlug}?resource-id=${resourceId}`
+                );
+                window.window.location = url;
+            };
+        }
+
+    ko.components.register('dashboard-card', {
+        viewModel: DashboardCardViewModel,
+        template: dashboardCardTemplate
+    })
+})
