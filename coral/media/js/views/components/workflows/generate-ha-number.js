c693bc278a108b5de671cce47955af72fd32c692
Taylor North <135817240+taylorn01@users.noreply.github.com>
2024-08-29 16:20:08 +0100

    fix(#2315): had to do some funkyness to get the value errors to display a banner to the user

--- coral/media/js/views/components/workflows/generate-ha-number.js
+++ coral/media/js/views/components/workflows/generate-ha-number.js
@@ -5,15 +5,17 @@ define([
   'uuid',
   'arches',
   'views/components/workflows/workflow-builder-initial-step',
+  'viewmodels/card-component',
   'templates/views/components/workflows/generate-ha-number.htm'
-], function (_, ko, koMapping, uuid, arches, WorkflowBuilderInitialStep, template) {
+], function (_, ko, koMapping, uuid, arches, WorkflowBuilderInitialStep, CardComponentViewModel, template) {
   function viewModel(params) {
     WorkflowBuilderInitialStep.apply(this, [params]);
+    this.generateHaNumber = {};
+    CardComponentViewModel.apply(this.generateHaNumber, [params]);
 
     this.SYSTEM_REFERENCE_RESOURCE_ID_NODE_ID = '325a430a-efe4-11eb-810b-a87eeabdefba';
 
     this.generateId = async () => {
-      if (ko.unwrap(ko.unwrap(this.tile().data[this.SYSTEM_REFERENCE_RESOURCE_ID_NODE_ID])?.en?.value)) return;
       params.pageVm.loading(true);
 
       const data = {
