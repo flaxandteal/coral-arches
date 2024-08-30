4b67dd68009bfbeb5bc49595788ba0397cfe5ea6
Taylor North <135817240+taylorn01@users.noreply.github.com>
2024-08-30 15:59:43 +0100

    fix(#2315): garden number component is now up to speed with smr and hb

--- coral/media/js/views/components/workflows/generate-garden-number.js
+++ coral/media/js/views/components/workflows/generate-garden-number.js
@@ -10,18 +10,32 @@ define([
   function viewModel(params) {
     CardComponentViewModel.apply(this, [params]);
 
-    this.gardenNumber = ko.observable('');
-
     this.ADDRESS_NODEGROUP = '87d39b25-f44f-11eb-95e5-a87eeabdefba';
     this.COUNTY_NODE_ID = '8bfe714e-3ec2-11ef-9023-0242ac140007';
     this.GENERATED_GARDEN_NODE_ID = 'bd85cca2-49a4-11ef-94a5-0242ac120007';
 
+    this.setValue = (value) => {
+      const localisedValue = {
+        en: {
+          direction: 'ltr',
+          value: value
+        }
+      };
+      if (ko.isObservable(this.tile.data[this.GENERATED_GARDEN_NODE_ID])) {
+        this.tile.data[this.GENERATED_GARDEN_NODE_ID](localisedValue);
+      } else {
+        this.tile.data[this.GENERATED_GARDEN_NODE_ID] = ko.observable();
+        this.tile.data[this.GENERATED_GARDEN_NODE_ID](localisedValue);
+      }
+    };
+
     this.getValue = () => {
-      return ko.unwrap(this.tile.data[this.GENERATED_GARDEN_NODE_ID])?.en?.value;
+      return (
+        ko.unwrap(ko.unwrap(this.tile.data[this.GENERATED_GARDEN_NODE_ID])?.['en']?.['value']) || ''
+      );
     };
 
     this.initialNumber = ko.observable(this.getValue());
-
     this.selectedCounty = ko.observable();
     this.errorMessage = 'No county has been selected';
 
@@ -43,9 +57,7 @@ define([
 
     this.getCountyID = async () => {
       const tile = await this.fetchCountyTile();
-      console.log('tile: ', tile)
       if (tile) {
-        console.log('p2');
         const countyName = tile.data[this.COUNTY_NODE_ID];
         if (countyName) {
           this.selectedCounty(countyName);
@@ -56,17 +68,6 @@ define([
         }
       }
     };
-
-    this.hasSelectedCounty = ko.computed(() => {
-      return !!this.selectedCounty();
-    }, this);
-
-    this.hasRemovedCounty = ko.computed(() => {
-      console.log('t1 ', this.selectedCounty(), this.initialNumber());
-      return !this.selectedCounty() && this.initialNumber();
-    }, this);
-
-    this.hasRemovedCounty.subscribe((value) => console.log('value: ', value))
 
     this.generateGardenNumber = async () => {
       if (!this.selectedCounty) return;
@@ -90,19 +91,14 @@ define([
       params.pageVm.loading(false);
     };
 
-    this.setValue = (value) => {
-      const localisedValue = {
-        en: {
-          direction: 'ltr',
-          value: value
-        }
-      };
-      if (ko.isObservable(this.tile.data[this.GENERATED_GARDEN_NODE_ID])) {
-        this.tile.data[this.GENERATED_GARDEN_NODE_ID](localisedValue);
-      } else {
-        this.tile.data[this.GENERATED_GARDEN_NODE_ID] = localisedValue;
-      }
-    };
+    this.hasSelectedCounty = ko.computed(() => {
+      return !!this.selectedCounty();
+    }, this);
+
+    this.hasRemovedCounty = ko.computed(() => {
+      console.log('t1 ', this.selectedCounty(), this.initialNumber());
+      return !this.selectedCounty() && this.initialNumber();
+    }, this);
 
     this.getCountyID();
   }
