0ea271fc89a96d97a082946711bbf60737a62f3f
Taylor North <135817240+taylorn01@users.noreply.github.com>
2024-10-03 15:25:47 +0100

    feat(#1706): implemented licence report page

---
+++ coral/media/js/views/components/reports/licence.js
@@ -0,0 +1,38 @@
+define([
+  'jquery',
+  'underscore',
+  'knockout',
+  'arches',
+  'utils/resource',
+  'utils/report',
+  'templates/views/components/reports/licence.htm',
+  'views/components/reports/scenes/name',
+  'views/components/reports/scenes/json'
+], function($, _, ko, arches, resourceUtils, reportUtils, historicLandscapeCharacterizationReportTemplate) {
+  return ko.components.register('licence-report', {
+      viewModel: function(params) {
+          var self = this;
+          params.configKeys = ['tabs', 'activeTabIndex'];
+          this.configForm = params.configForm || false;
+          this.configType = params.configType || 'header';
+
+          Object.assign(self, reportUtils);
+          self.sections = [
+              {id: 'all', title: 'Full Report'},
+          ];
+          self.reportMetadata = ko.observable(params.report?.report_json);
+          self.resource = ko.observable(self.reportMetadata()?.resource);
+          self.displayname = ko.observable(ko.unwrap(self.reportMetadata)?.displayname);
+          self.activeSection = ko.observable('name');
+          self.historicLandscapeClassificationPhase = ko.observableArray();
+          self.print = ko.observable(window.location.href.indexOf("?print") > -1)
+
+          self.fullReportConfig = {
+              id: 'historic-landscape-characterization',
+              label: 'Historic Landscape Characterization',
+              ignoreNodes: []
+          }
+      },
+      template: historicLandscapeCharacterizationReportTemplate
+  });
+});
