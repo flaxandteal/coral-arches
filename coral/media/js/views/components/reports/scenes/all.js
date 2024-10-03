a5302b55584a52507c79e7a0857e938fe3a5a632
Taylor North <135817240+taylorn01@users.noreply.github.com>
2024-10-03 13:55:29 +0100

    feat(#1706): the all report is now formatting the data correctly for the summary component to handle rendering the values

--- coral/media/js/views/components/reports/scenes/all.js
+++ coral/media/js/views/components/reports/scenes/all.js
@@ -1,115 +1,180 @@
 define([
-    'underscore',
-    'knockout',
-    'arches',
-    'utils/report',
-    'templates/views/components/reports/scenes/all.htm',
-    'bindings/datatable',
-    'views/components/reports/scenes/all'
-], function(_, ko, arches, reportUtils, allReportTemplate) {
-    return ko.components.register('views/components/reports/scenes/all', {
-        viewModel: function(params) {
-            var self = this;
-            self.table = ko.observable(params.table)
-            self.margin = ko.observable(params.margin && !params.table ? params.margin : 0)
-            Object.assign(self, reportUtils);
+  'underscore',
+  'knockout',
+  'arches',
+  'utils/report',
+  'views/components/workflows/summary-step',
+  'templates/views/components/reports/scenes/all.htm',
+  'bindings/datatable',
+  'views/components/workflows/render-nodes'
+  // 'views/components/reports/scenes/all'
+], function (_, ko, arches, reportUtils, SummaryStep, allReportTemplate) {
+  return ko.components.register('views/components/reports/scenes/all', {
+    viewModel: function (params) {
+      params.resourceid = params.resourceInstanceId;
+      params.pageVm = {
+        plugin: { slug: 'ignore' }
+      };
+      SummaryStep.apply(this, [params]);
 
-            self.nameTableConfig = {
-                ...self.defaultTableConfig,
-                "columns": [
-                    { "width": "50%" },
-                    { "width": "20%" },
-                    { "width": "20%" },
-                   null,
-                ]
-            };
+      this.showReport = ko.observable(false);
 
-            this.crossReferenceTableConfig = {
-                ...this.defaultTableConfig,
-                "columns": [
-                    { "width": "20%" },
-                    { "width": "20%" },
-                    { "width": "50%" },
-                    { "width": "10%" },
-                   null,
-                ]
-            };
-            this.resource = params.data
-            this.cards = Object.keys(this.resource)
-            this.cards.forEach(label => {
-                if (label.indexOf("@") !== -1 || label.indexOf("_") !== -1) {
-                    delete this.resource[label]
-                }
-            })
+      this.processGroup = (key, group, nodeConfig) => {
+        const lowerCaseKey = key.toLowerCase();
+        if (lowerCaseKey in nodeConfig) {
+          return;
+        }
+        if (Array.isArray(group)) {
+          nodeConfig[lowerCaseKey] = {
+            label: key,
+            nodegroupId: group[0]['@node_id'],
+            renderNodes: this.processNodes(group[0], nodeConfig)
+          };
+          return;
+        }
+        nodeConfig[lowerCaseKey] = {
+          label: key,
+          nodegroupId: group['@node_id'],
+          renderNodes: this.processNodes(group, nodeConfig)
+        };
+        return nodeConfig;
+      };
 
-            this.filterValues = (valueObject) => {
-                let values = {}
-                let metaless = Object.keys(valueObject)
-                .filter(vo => vo.indexOf("@") === -1 && vo.indexOf("_") === -1)
+      this.processNodes = (nodes, nodeConfig) => {
+        const renderNodes = [];
+        Object.entries(nodes).forEach(([key, value]) => {
+          if (key.startsWith('@')) {
+            return;
+          }
+          if (
+            Object.prototype.toString.call(value) !== '[object Object]' ||
+            !('@parent_node_id' in value)
+          ) {
+            renderNodes.push(value['@node_id']);
+            return;
+          }
+          this.processGroup(key, value, nodeConfig);
+        });
+        return renderNodes;
+      };
 
-                metaless
-                .map(key => {
-                    values[key] = {}
-                    if (Array.isArray(valueObject[key])) {
-                        values[key].cardinality = "m"
-                        values[key].data = []
-                        valueObject[key].forEach(row => {
-                            if (Object.keys(row).includes("@display_value")) {
-                                values[key].data.push(row["@display_value"])
-                            } else {
-                                values[key].data.push(row)
-                            }
-                        })
-                        
-                    } else {
-                        values[key].cardinality = "1"
-                        if (Object.keys(valueObject[key]).includes("@display_value")) {
-                            values[key].data = valueObject[key]["@display_value"]
-                        } else {
-                            values[key].data = valueObject[key]
-                        }
-                    }
-                })
-                return values
-            }
+      this.getData = async () => {
+        const nodeConfig = {
+          id: 'heritage-asset',
+          label: 'Heritage Asset'
+        };
 
-            this.sections = {}
-            this.cards.forEach(title => {
-                this.sections[title] = {}
-                if (Array.isArray(this.resource[title])) {
-                    this.sections[title].cardinality = "m"
-                    this.sections[title].data = []
-                    this.resource[title].forEach(row => {
-                        if (row) {
-                            if (Object.keys(row).includes("@display_value")) {
-                                this.sections[title].data.push(row["@display_value"])
-                            } else {
-                                this.sections[title].data.push(row)
-                            }
-                        }
-                    })
-                } else {
-                    this.sections[title].cardinality = "1"
-                    if (this.resource[title]) {
-                        if (Object.keys(this.resource[title]).includes("@display_value")) {
-                            this.sections[title].data = this.resource[title]["@display_value"]
-                        } else {
-                            this.sections[title].data = this.resource[title]
-                        }
-                    }
-                }
-            })
+        Object.entries(params.data).forEach(([key, value]) => {
+          this.processGroup(key, value, nodeConfig);
+        });
 
+        await this.renderResourceIds(this.resourceid, nodeConfig);
 
+        console.log('HA Summary: ', this.renderedNodegroups());
+        this.showReport(true);
+      };
 
-            self.dataConfig = {
-            }
-            Object.assign(self.dataConfig, params.dataConfig || {});
+      this.loadData();
 
-            // if params.compiled is set and true, the user has compiled their own data.  Use as is.
-            
+      // var self = this;
+      // self.table = ko.observable(params.table)
+      // self.margin = ko.observable(params.margin && !params.table ? params.margin : 0)
+      // Object.assign(self, reportUtils);
 
-        },
-        template: allReportTemplate
-    });
+      // self.nameTableConfig = {
+      //     ...self.defaultTableConfig,
+      //     "columns": [
+      //         { "width": "50%" },
+      //         { "width": "20%" },
+      //         { "width": "20%" },
+      //        null,
+      //     ]
+      // };
+
+      // this.crossReferenceTableConfig = {
+      //     ...this.defaultTableConfig,
+      //     "columns": [
+      //         { "width": "20%" },
+      //         { "width": "20%" },
+      //         { "width": "50%" },
+      //         { "width": "10%" },
+      //        null,
+      //     ]
+      // };
+      //   this.resource = params.data
+      //   // console.log('resource: ', this.resource)
+
+      //   this.cards = Object.keys(this.resource)
+      //   this.cards.forEach(label => {
+      //       if (label.indexOf("@") !== -1 || label.indexOf("_") !== -1) {
+      //           delete this.resource[label]
+      //       }
+      //   })
+
+      // this.filterValues = (valueObject) => {
+      //     let values = {}
+      //     let metaless = Object.keys(valueObject)
+      //     .filter(vo => vo.indexOf("@") === -1 && vo.indexOf("_") === -1)
+
+      //     metaless
+      //     .map(key => {
+      //         values[key] = {}
+      //         if (Array.isArray(valueObject[key])) {
+      //             values[key].cardinality = "m"
+      //             values[key].data = []
+      //             valueObject[key].forEach(row => {
+      //                 if (Object.keys(row).includes("@display_value")) {
+      //                     values[key].data.push(row["@display_value"])
+      //                 } else {
+      //                     values[key].data.push(row)
+      //                 }
+      //             })
+
+      //         } else {
+      //             values[key].cardinality = "1"
+      //             if (Object.keys(valueObject[key]).includes("@display_value")) {
+      //                 values[key].data = valueObject[key]["@display_value"]
+      //             } else {
+      //                 values[key].data = valueObject[key]
+      //             }
+      //         }
+      //     })
+      //     return values
+      // }
+
+      //   this.sections = {}
+      //   this.cards.forEach(title => {
+      //       this.sections[title] = {}
+      //       if (Array.isArray(this.resource[title])) {
+      //           this.sections[title].cardinality = "m"
+      //           this.sections[title].data = []
+      //           this.resource[title].forEach(row => {
+      //               if (row) {
+      //                   if (Object.keys(row).includes("@display_value")) {
+      //                       this.sections[title].data.push(row["@display_value"])
+      //                   } else {
+      //                       this.sections[title].data.push(row)
+      //                   }
+      //               }
+      //           })
+      //       } else {
+      //           this.sections[title].cardinality = "1"
+      //           if (this.resource[title]) {
+      //               if (Object.keys(this.resource[title]).includes("@display_value")) {
+      //                   this.sections[title].data = this.resource[title]["@display_value"]
+      //               } else {
+      //                   this.sections[title].data = this.resource[title]
+      //               }
+      //           }
+      //       }
+      //   })
+
+      // self.dataConfig = {
+      // }
+      // Object.assign(self.dataConfig, params.dataConfig || {});
+
+      // if params.compiled is set and true, the user has compiled their own data.  Use as is.
+    },
+    template: allReportTemplate
+  });
 });
