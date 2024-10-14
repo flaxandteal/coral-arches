d90daa59cbe3450e81c04de92816eddda027c779
Stuart Marshall <stuart.marshall@flaxandteal.co.uk>
2024-10-14 11:28:29 +0100

    version: v5.17.50

--- coral/settings.py
+++ coral/settings.py
@@ -22,7 +22,7 @@ except ImportError:
     pass
 
 APP_NAME = 'coral'
-APP_VERSION = semantic_version.Version(major=5, minor=17, patch=49)
+APP_VERSION = semantic_version.Version(major=5, minor=17, patch=50)
 
 GROUPINGS = {
     "groups": {
