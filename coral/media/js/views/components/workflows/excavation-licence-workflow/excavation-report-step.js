1956e9eef9ab582732d57c81525880ff05a86d2c
Stuart Marshall <stuart.marshall@flaxandteal.co.uk>
2024-10-15 15:30:32 +0100

    feat(#2608): add check for classification date for disableAdd

--- coral/media/js/views/components/workflows/excavation-licence-workflow/excavation-report-step.js
+++ coral/media/js/views/components/workflows/excavation-licence-workflow/excavation-report-step.js
@@ -9,19 +9,30 @@ define([
 ], function (_, ko, koMapping, uuid, arches, DefaultCardUtilViewModel, componentTemplate) {
   function viewModel(params) {
 
-  DefaultCardUtilViewModel.apply(this, [params]);
+    DefaultCardUtilViewModel.apply(this, [params]);
 
-  this.DOCUMENT_REFERENCE_NODE = "d8f74d42-dc6e-11ee-8def-0242ac120006"
+    this.DOCUMENT_REFERENCE_NODE = "d8f74d42-dc6e-11ee-8def-0242ac120006"
+    this.CLASSIFICATION_DATE_NODE = "ea6ea7a8-dc70-11ee-b70c-0242ac120006"
 
-      params.disableAdd(true)
+    params.disableAdd(true)
 
-      this.tile.data[this.DOCUMENT_REFERENCE_NODE].subscribe((newValue) => {
-          if (newValue.en.value != ""){
+    
 
-              params.disableAdd(false)
-          }
-      })
+    const checkBothNodes = (classificationDate, docReference) => {
+      if (classificationDate !== null && ko.unwrap(docReference) !== ''){
+        params.disableAdd(false)
+      }
+    }
 
+    this.tile.data[this.CLASSIFICATION_DATE_NODE].subscribe((newValue) => {
+      const documentReferenceValue = this.tile.data[this.DOCUMENT_REFERENCE_NODE]().en.value;
+      checkBothNodes(newValue, documentReferenceValue);
+    });
+
+    this.tile.data[this.DOCUMENT_REFERENCE_NODE].subscribe((newValue) => {
+      const classificationDateValue = this.tile.data[this.CLASSIFICATION_DATE_NODE]();
+      checkBothNodes(classificationDateValue, newValue.en.value);
+    });
 
   }
 
