5f1deef0f5f3191e64adf0bc4722db6c77ded95e
owen <owen@galviadigital.com>
2024-10-25 09:30:37 +0100

    version: v6.0.0

--- coral/settings.py
+++ coral/settings.py
@@ -22,7 +22,7 @@ except ImportError:
     pass
 
 APP_NAME = 'coral'
-APP_VERSION = semantic_version.Version(major=5, minor=21, patch=64)
+APP_VERSION = semantic_version.Version(major=6, minor=0, patch=0)
 
 GROUPINGS = {
     "groups": {
