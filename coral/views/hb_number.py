213fa69899e3bc245abd4a14a841dcc24b89eafe
Stuart Marshall <stuart.marshall@flaxandteal.co.uk>
2024-11-01 12:43:29 +0000

    feat(#2626): get request for HB numbers

--- coral/views/hb_number.py
+++ coral/views/hb_number.py
@@ -4,6 +4,7 @@ from arches.app.models.tile import Tile
 from arches.app.utils.response import JSONResponse
 from coral.utils.hb_number import HbNumber
 from arches.app.models import models
+import re
 
 
 HERITAGE_ASSET_REFERENCES_NODEGROUP_ID = "e71df5cc-3aad-11ef-a2d0-0242ac120003"
@@ -11,6 +12,31 @@ HB_NUMBER_NODE_ID = "250002fe-3aae-11ef-91fd-0242ac120003"
 
 
 class HbNumberView(View):
+    def get(self, request):
+        query = {f"data__{HB_NUMBER_NODE_ID}__en__value__regex": r'^HB\d'}
+        try:
+            hb_number_tile = Tile.objects.filter(
+                nodegroup_id=HERITAGE_ASSET_REFERENCES_NODEGROUP_ID,
+                **query,
+            )
+        except Exception as e:
+            print(f"Failed to find any existing HB numbers: {e}")
+            return 
+        
+        hb_numbers = [
+            tile.data.get(HB_NUMBER_NODE_ID, {}).get('en', {}).get('value', None)
+            for tile in hb_number_tile
+            if tile.data.get(HB_NUMBER_NODE_ID, {}).get('en', {}).get('value', None) is not None
+        ]
+
+        hb_numbers = [re.sub(r'[a-zA-Z]+$', '', num) for num in hb_numbers]
+
+        hb_numbers = list(set(hb_numbers))
+
+        response = [{'text': num, 'id': num} for num in hb_numbers]
+
+        return JSONResponse({"hbNumbers": response})
+    
     def post(self, request):
         data = json.loads(request.body.decode("utf-8"))
         resource_instance_id = data.get("resourceInstanceId")
