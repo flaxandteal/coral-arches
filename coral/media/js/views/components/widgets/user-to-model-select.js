3418b96e97cce70ade7271e69f919a9c8e9d2b86
owen <owen@galviadigital.com>
2024-11-12 15:36:59 +0000

    feat(#2154): added a sign off widget

--- coral/media/js/views/components/widgets/user-to-model-select.js
+++ coral/media/js/views/components/widgets/user-to-model-select.js
@@ -4,78 +4,122 @@ define([
   'arches',
   'uuid',
   'underscore',
-    'viewmodels/card-component',
+  'viewmodels/card-component',
   'templates/views/components/widgets/user-to-model-select.htm'
 ], function ($, ko, arches, uuid, _, CardViewModel, template) {
   const viewModel = function (params) {
-    CardViewModel.apply(this, [params]);
-
     const ResourceInstanceSelectViewModel = require('viewmodels/resource-instance-select');
-
-    params.multiple = true;
-    params.datatype = 'resource-instance';
 
     ResourceInstanceSelectViewModel.apply(this, [params]);
 
-    this.signOffNodes = ko.observable({
-      "42ec3232-fb26-11ee-838d-0242ac190006": {
-        "groupsRequired" : [
-
-        ],
-        "value": ko.observable(false)
-      },
-      "998d6728-fb26-11ee-838d-0242ac190006": {
-        "groupsRequired" : [
-
-        ],
-        "value": ko.observable(false)
-      }
-    })
-
     this.params = params
-    console.log("yolo")
-    console.dir(this)
+    this.name = ko.observable('')
+    this.personId = ko.observable('')
+    this.permittedToSign = ko.observable(!!this.tile.data[this.node.id]())
+    this.isUserAlreadyAdded = !!this.tile.data[this.node.id]()
+    this.isUserAdded = ko.observable(!!this.tile.data[this.node.id]())
+    this.valueString = ko.observable(this.tile.data[this.node.id]() ? this.tile.data[this.node.id]() : 'loading...')
+    this.iconClass = ko.observable(this.permittedToSign() ? (this.isUserAdded() ? 'fa fa-check' : 'fa-plus-circle') : 'fa fa-code')
+    this.textClass = ko.observable(this.isUserAlreadyAdded ? '' : 'cons-type')
 
     this.setValue = () => {
-      console.log("triggered")
-      this.tile.data[this.nodeid]([{resourceId: '555ff689-1957-4ae7-a772-fc49bc49b76f'}])
+      if (this.tile.data[this.node.id]()) {
+        this.tile.data[this.node.id](null)
+        this.iconClass('fa fa-plus-circle')
+        this.textClass('cons-type')
+      } else {
+        this.tile.data[this.node.id]([{resourceId: this.personId()}])
+        this.iconClass('fa fa-check')
+        this.textClass('')
+        }
     }
 
-    this.isUserAlreadyAdded = ko.computed(() => {
-      const userAddedDict = {}
-      Object.keys(this.signOffNodes())
-      .forEach(signatureNode => {
-        userAddedDict[signatureNode] = this.tile.data[signatureNode]() ? true : false
-      })
-      return userAddedDict;
-    }, this);
+    this.tile.data[this.node.id].subscribe(data => {
+      if (!data) {
+        this.isUserAdded(false)
+        this.iconClass(this.permittedToSign() ? 'fa-plus-circle' : 'fa fa-code')
+      }
+      this.isUserAdded(true)
+      this.iconClass(this.permittedToSign() ? 'fa fa-check' : 'fa fa-code')
+    })
 
-    this.confirmSignature = (nodeid, person) => {
-      console.log(nodeid, person)
-    }
+    this.signOffGroups = params.signOffGroups ? params.signOffGroups : [] 
 
-    this.addUserToResourceSelect = async (nodeid) => {
-      console.log('adding user to resource select');
-
+    this.getPersonId = async () => {
       const response = await $.ajax({
         type: 'GET',
         url: '/user-to-model',
         dataType: 'json',
         context: this,
         error: (response, status, error) => {
-          console.log(response, status, error);
+          console.error(response, status, error);
         }
       });
+      const personId = response.person.resource_id
+      this.personId(personId)
+      return personId
+    }
 
-      this.confirmSignature(nodeid, response.person)
+    this.getName = async () => {
+      const personId = this.personId() ? this.personId() : await this.getPersonId()
+      const name = await $.ajax({
+        type: 'GET',
+        url: `/resource/descriptors/${personId}`,
+        dataType: 'json',
+        context: this,
+        error: (response, status, error) => {
+          console.error(response, status, error);
+        }
+      })
+      this.name(name.displayname)
+      this.valueString(name.displayname)
+      return name.displayname
+    }
+
+    this.validatePermission = async () => {
+      const personId = this.personId() ? this.personId() : await this.getPersonId() 
+      const groups = await $.ajax({
+        type: 'GET',
+        url: `/search/resources?advanced-search=[{"op"%3A"and"%2C"bb2f7e1c-7029-11ee-885f-0242ac140008"%3A{"op"%3A""%2C"val"%3A["${personId}"]}}]`,
+        dataType: 'json',
+        context: this,
+        error: (response, status, error) => {
+          console.error(response, status, error);
+        }
+      })
+
+      if (this.signOffGroups.length > 0) {
+        groupIntersection = Array.from(new Set(groups.results.hits.hits.map(hit => hit._id)).intersection(new Set(this.signOffGroups)))
+        this.permittedToSign(groupIntersection.length > 0)
+        return groupIntersection.length > 0
+      } 
+      return false
     };
+
+    this.initialize = async () => {
+      const permitted = await this.validatePermission()
+      if (permitted) {
+        this.valueString(this.getName())
+      } else {
+        this.valueString('You do not have permission to sign off')
+      }
+    }
+    if (!this.isUserAlreadyAdded) {
+      this.initialize()
+    } else {
+      this.valueString(this.tile.data[this.node.id]()[0].resourceName())
+      this.tile.data[this.node.id]()[0].resourceName.subscribe(name => {
+        this.valueString(name)
+      })
+    }
   };
 
   /**
-   * registers a text-widget component for use in forms
+   * registers a sign-off component for use in forms
    * @function external:"ko.components".check-open-applications
    * @param {object} params
    * @param {string} params.value - the value being managed
+   * @param {list} signOffGroups - groups which have permission to sign
    * @param {function} params.config - observable containing config object
    * @param {string} params.config().label - label to use alongside the text input
    * @param {string} params.config().placeholder - default text to show in the text input
