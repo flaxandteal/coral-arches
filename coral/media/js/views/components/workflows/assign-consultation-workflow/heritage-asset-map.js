2dcfa2b45913f31eddca3fc9b05cfe9fe862574c
owen <owen@galviadigital.com>
2025-01-21 09:02:43 +0000

    wip: map geometries should be updating

--- coral/media/js/views/components/workflows/assign-consultation-workflow/heritage-asset-map.js
+++ coral/media/js/views/components/workflows/assign-consultation-workflow/heritage-asset-map.js
@@ -8,7 +8,7 @@ define([
     'viewmodels/card-component',
     'utils/map-configurator',
     'utils/map-popup-provider',
-    // 'viewmodels/map',
+    'viewmodels/map',
     'mapbox-gl',
     'mapbox-gl-geocoder',
     'templates/views/components/workflows/assign-consultation-workflow/heritage-asset-map.htm'
@@ -22,28 +22,24 @@ define([
     CardComponentViewModel,
     mapConfigurator,
     mapPopupProvider,
+    MapViewModel,
     MapboxGl,
     MapboxGeocoder,    
     template
   ) {
     function viewModel(params) {
-      const MapViewModel = require('viewmodels/map');
       this.loading = params.pageVm.loading;
       const self = this;
-      const geoTiles = ko.observable([])
-      geoTiles.subscribe((geometries) => {
-        updateMap(geometries, this.map)
-      })
+      const dontUpdate = ko.observable({})
+      const featureIndex = ko.observable(0)
+
       this.RELATED_HERITAGE_NODE_ID = 'bc64746e-cf4a-11ef-997c-0242ac120007';
       this.GEO_NODE_ID = "87d3872b-f44f-11eb-bd0c-a87eeabdefba";
-
+      
       MapViewModel.apply(self, [params]);
-
-
 
       this.setupMap = function (map) {
         map.on('load', function () {
-          require(['mapbox-gl', 'mapbox-gl-geocoder'], function (MapboxGl, MapboxGeocoder) {
             mapConfigurator.preConfig(map);
             map.addControl(new MapboxGl.NavigationControl(), 'top-left');
             map.addControl(
@@ -110,27 +106,16 @@ define([
 
             mapConfigurator.postConfig(map);
             self.map(map);
-            
-            this.map = map
-
-          });
         });
+        getGeometryTiles()
       };
 
-
-
-
-      getGeometryTiles = async () => {
-        const geometries = {};
-
-        await Promise.all(
-          [params.baseResourceId].map((resourceId) => {
-            if (!resourceId) return;
-            return $.ajax({
+      const getGeometryTiles = async () => {
+        await $.ajax({
               type: 'GET',
               url:
                 arches.urls.root +
-                `resource/${resourceId}/tiles?nodeid=${this.RELATED_HERITAGE_NODE_ID}`,
+                `resource/${params.baseResourceId}/tiles?nodeid=${this.RELATED_HERITAGE_NODE_ID}`,
               context: this,
               success: async (responseText, status, response) => {
                 const tiles =
@@ -143,26 +128,22 @@ define([
               },
               error: (response, status, error) => {
                 if (response.statusText !== 'abort') {
-                  this.viewModel.alert(
-                    new AlertViewModel(
-                      'ep-alert-red',
-                      arches.requestFailed.title,
-                      response.responseText
-                    )
-                  );
+                  // this.viewModel.alert(
+                  //   new AlertViewModel(
+                  //     'ep-alert-red',
+                  //     arches.requestFailed.title,
+                  //     response.responseText
+                  //   )
+                  // );
                 }
               }
             });
-          })
-        );
         tilesArray.forEach((tile, idx) => {
-          geometries[idx] = getGeoJsonData(tile.resourceId);
+          getGeoJsonData(tile.resourceId);
         });
-          return geometries;
       };
-      const getGeoJsonData = (resourceId) => {
-        const returnFeature = {};
 
+      const getGeoJsonData = (resourceId) => {
         $.ajax({
           type: 'GET',
               url:
@@ -173,85 +154,92 @@ define([
 
                 let filteredResponseData = response.responseJSON.tiles[0].display_values['1'].value;
                 let featuresObject = JSON.parse(filteredResponseData.replace(/'/g, '"'))
-
-                returnFeature[0] = featuresObject['features'];
-                geoTiles(returnFeature[0])
+                if (dontUpdate()[featuresObject.features[0].id]) {
+                  return
+                } 
+                if (self.map()) {
+                  dontUpdate({...dontUpdate(), [featuresObject.features[0].id] : true})
+                }
+                  updateMap(featuresObject['features'])
               },
               error: (response, status, error) => {
               }
         });
+    };
 
-        return returnFeature[0];
-      };
-      getGeometryTiles()
-    }
+    const updateMap = (geometries) => {
+      let map = self.map()
 
-    const updateMap = (geometries, map) => {
       let bounds = new MapboxGl.LngLatBounds();
-        Object.values(geometries).forEach((geometry, idx) => {
-          let colour = '#0000FF';
-          if (idx <= 0) colour = '#FF0000';
-            if (geometry.geometry.type === 'Point') {
-              bounds.extend(geometry.geometry.coordinates);
-            } else if (
-              geometry.geometry.type === 'Polygon' ||
-              geometry.geometry.type === 'MultiPolygon'
-            ) {
-              geometry.geometry.coordinates.forEach(function (polygon) {
-                polygon.forEach(function (coord) {
-                  bounds.extend(coord);
-                });
-              });
-            } else if (
-              geometry.geometry.type === 'LineString' ||
-              geometry.geometry.type === 'MultiLineString'
-            ) {
-              geometry.geometry.coordinates.forEach(function (coord) {
+      Object.values(geometries).forEach((geometry, idx) => {
+        let colour = '#0000FF';
+        if (idx <= 0) colour = '#FF0000';
+          if (geometry.geometry.type === 'Point') {
+            bounds.extend(geometry.geometry.coordinates);
+          } else if (
+            geometry.geometry.type === 'Polygon' ||
+            geometry.geometry.type === 'MultiPolygon'
+          ) {
+            geometry.geometry.coordinates.forEach(function (polygon) {
+              polygon.forEach(function (coord) {
                 bounds.extend(coord);
               });
-            }
-            map.addSource('myGeoJSON' + idx + j + 1, {
-              type: 'geojson',
-              data: geometry
             });
-            map.addLayer({
-              id: 'myGeoJSON-polygon-layer' + idx + j + 1,
-              type: 'fill',
-              source: 'myGeoJSON' + idx + j + 1,
-              'source-layer': '',
-              paint: {
-                'fill-color': colour,
-                'fill-opacity': 0.5
-              },
-              filter: ['==', '$type', 'Polygon']
+          } else if (
+            geometry.geometry.type === 'LineString' ||
+            geometry.geometry.type === 'MultiLineString'
+          ) {
+            geometry.geometry.coordinates.forEach(function (coord) {
+              bounds.extend(coord);
             });
-            map.addLayer({
-              id: 'myGeoJSON-circle-layer' + idx + j + 1,
-              type: 'circle',
-              source: 'myGeoJSON' + idx + j + 1,
-              'source-layer': '',
-              paint: {
-                'circle-color': colour,
-                'circle-radius': 6,
-                'circle-opacity': 0.5
-              },
-              filter: ['==', '$type', 'Point']
-            });
-            map.addLayer({
-              id: 'myGeoJSON-line-layer' + idx + j + 1,
-              type: 'line',
-              source: 'myGeoJSON' + idx + j + 1,
-              'source-layer': '',
-              paint: {
-                'line-color': colour,
-                'line-width': 2,
-                'line-opacity': 0.5
-              },
-              filter: ['==', '$type', 'LineString']
-            });
-            map.fitBounds(bounds, { padding: 20 });
-        });
+          }
+          map.addSource(geometry.id, {
+            type: 'geojson',
+            data: geometry
+          });
+          map.addLayer({
+            id: 'myGeoJSON-polygon-layer' + geometry.id,
+            type: 'fill',
+            source: geometry.id,
+            'source-layer': '',
+            paint: {
+              'fill-color': colour,
+              'fill-opacity': 0.5
+            },
+            filter: ['==', '$type', 'Polygon']
+          });
+          map.addLayer({
+            id: 'myGeoJSON-circle-layer' + geometry.id,
+            type: 'circle',
+            source: geometry.id,
+            'source-layer': '',
+            paint: {
+              'circle-color': colour,
+              'circle-radius': 6,
+              'circle-opacity': 0.5
+            },
+            filter: ['==', '$type', 'Point']
+          });
+          map.addLayer({
+            id: 'myGeoJSON-line-layer' + geometry.id,
+            type: 'line',
+            source: geometry.id,
+            'source-layer': '',
+            paint: {
+              'line-color': colour,
+              'line-width': 2,
+              'line-opacity': 0.5
+            },
+            filter: ['==', '$type', 'LineString']
+          });
+          featureIndex(featureIndex() + 1)
+          map.fitBounds(bounds, { padding: 20 });
+          self.map(map)
+      });
     }
+    }
+
+    
   
     ko.components.register('heritage-asset-map', {
       viewModel: viewModel,
