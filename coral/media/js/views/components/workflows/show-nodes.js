9f2a50d1ccdc51f02f095fc0a84f3babc2f8aa98
Stuart Marshall <stuart.marshall@flaxandteal.co.uk>
2025-02-05 13:54:59 +0000

    fix: townlands not showing correct value
    #2883

--- coral/media/js/views/components/workflows/show-nodes.js
+++ coral/media/js/views/components/workflows/show-nodes.js
@@ -7,22 +7,20 @@ define([
     'viewmodels/card-component',
     'viewmodels/alert',
     'templates/views/components/workflows/show-nodes.htm'
-  ], function (_, ko, koMapping, uuid, arches, CardComponentViewModel, AlertViewModel, template) {
+], function(_, ko, koMapping, uuid, arches, CardComponentViewModel, AlertViewModel, template) {
     function viewModel(params) {
-        console.log("I'm trying to show nodes")
-        console.log(params)
-        console.log(params.showNodes)
-        console.log("Yo bro", Object.values(params.tile.data)[0]())
-        console.log("Yo bro", Object.values(params.tile.data)[0]()[0]["resourceId"])
-        resourceId = Object.values(params.tile.data)[0]()[0]["resourceId"]
 
-        showNodes = ko.observable(params.showNodes)
-        decodeHTML = (input) => {
+        const self = this;
+        self.resourceId = Object.values(params.tile.data)[0]()[0]["resourceId"];
+        self.title = params.title;
+        console.log(params.title, self.title)
+        self.showNodes = ko.observable(params.showNodes);
+        self.decodeHTML = (input) => {
             var doc = new DOMParser().parseFromString(input, "text/html");
             return doc.documentElement.textContent;
-        }
+        };
 
-        getORMData = async () => {
+        self.getORMData = async() => {
             const ormResources = await $.ajax({
                 type: 'POST',
                 url: '/orm/resources',
@@ -30,26 +28,45 @@ define([
                 data: {
                     show_nodes : params.showNodes.map(sn => sn[1]),
                     graphid : "076f9381-7b00-11e9-8d6b-80000b44d1d9",
-                    resourceid : resourceId
+                    resourceid : self.resourceId
                 },
                 context: this
             });
-            displayNodes = []
-            showNodes().forEach(labelValue => {
-              console.log()
-              displayNodes.push([labelValue[0], decodeHTML(ormResources[labelValue[1]])])
-            })
-            showNodes(displayNodes)
+            this.displayNodes = [];
+            for (let [key, value] of Object.entries(ormResources)){
+                if(value[0] === '['){
+                    const concept = self.parseConcept(value);
+                    ormResources[key] = concept.name;
+                }
+            }
+            self.showNodes().forEach(labelValue => {
+                console.log("here", labelValue);
+                this.displayNodes.push([labelValue[0], self.decodeHTML(ormResources[labelValue[1]])]);
+            });
+            self.showNodes(this.displayNodes);
 
-        }
-        getORMData()
+        };
+        self.getORMData();
+
+        self.parseConcept = (string) => {
+            const regex = /^\[([^>]+)>([^[]+)\[([^\]]+)\]\]$/;
+            const match = string.match(regex);
+            if (match) {
+                const concept = {
+                    id: match[1],
+                    conceptId: match[2],
+                    name: match[3]
+                };
+                return concept;
+            }
+        };
     }
   
     ko.components.register('show-nodes', {
-      viewModel: viewModel,
-      template: template
+        viewModel: viewModel,
+        template: template
     });
   
     return viewModel;
-  });
+});
   
