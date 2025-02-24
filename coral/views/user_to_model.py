8933dd03bd5fd05dc0f411ada73699d1657b9d49
owen <owen@galviadigital.com>
2025-02-24 15:19:35 +0000

    fix: adapt to get name from arches-orm release/2.0

--- coral/views/user_to_model.py
+++ coral/views/user_to_model.py
@@ -13,9 +13,11 @@ class UserToModel(View):
         name = None
         try:
             with admin():
-                person = Person.where(user_account=int(user.id)).get()
+                person = Person.where(user_account=int(user.id))
                 person = person[0] if len(person) else None 
-                name = person._.resource.descriptors['en']['name']
+                # descriptor was returning as None on arches-orm release/0.2 even though they worked on emerald.
+                # name = person._.resource.descriptors['en']['name']
+                name = person.name[0].full_name
         except Resource.DoesNotExist:
             person = None
 
