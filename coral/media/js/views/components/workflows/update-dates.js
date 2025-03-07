e9b3a16527c9924e40f6e02cebc776f68d41f77d
Dylan Cree <dylan.cree@flaxandteal.co.uk>
2025-03-07 10:21:19 +0000

    feat: added ability to specify amount of days to add to update-dates component

--- coral/media/js/views/components/workflows/update-dates.js
+++ coral/media/js/views/components/workflows/update-dates.js
@@ -12,12 +12,13 @@ define([
     const DATE_NODEGROUP_ID = params.dateNodeGroupID;
     const ISSUE_DATE_NODE = params.issueDateNode;
     const DUE_DATE_UNTIL_NODE = params.dueDateNode;
+    const DAYS_TO_ADD = params.daysToAdd;
     
     CardComponentViewModel.apply(this, [params]);
 
       if (this.tile.data[ISSUE_DATE_NODE] && ko.isObservable(this.tile.data[ISSUE_DATE_NODE])) {
         this.tile.data[ISSUE_DATE_NODE].subscribe(issueDate => {
-          const dueDate = this.addTwoWeeks(issueDate)
+          const dueDate = this.addDays(issueDate)
           this.tile.data[DUE_DATE_UNTIL_NODE](dueDate)
         })
       }
@@ -35,10 +36,10 @@ define([
       };
       
       
-      this.addTwoWeeks = (dateString) => {
+      this.addDays = (dateString) => {
         const date = new Date(dateString);
 
-        date.setDate(date.getDate() + 14);
+        date.setDate(date.getDate() + DAYS_TO_ADD);
       
         const year = date.getFullYear();
         const month = (date.getMonth() + 1).toString().padStart(2, '0');
