1e7b03f1514705a4f29d6eca40b3be17e99763e6
Stuart Marshall <stuart.marshall@flaxandteal.co.uk>
2025-03-24 14:12:40 +0000

    feat: update notification base strategy to use a config

--- coral/functions/notifications/notification_base_strategy.py
+++ coral/functions/notifications/notification_base_strategy.py
@@ -4,37 +4,52 @@ from arches_orm.adapter import admin
 import logging
 
 class NotificationStrategy():
-    def __init__(self, request):
+    def __init__(self, tile, request, user, config):
         self.request = request
+        self.tile = tile
+        self.resource_instance_id = str(tile.resourceinstance.resourceinstanceid)
+        self.user = user
+        self.config = config
+        self.name = self.get_resource_name(self.config.get('remove_prefix', None), self.config.get('remove_suffix', None))
+        self.notification = self.create_notification()
 
-    def send_notification(self, user, tile):
-        raise NotImplementedError("Subclasses should implement this method")
+    def send_notification(self):
+        check_prefix = self.config.get('check_prefix', None)
 
-    def get_resource_details(self, tile, remove_prefix=None, remove_suffix=None):
-        resource_instance_id = str(tile.resourceinstance.resourceinstanceid)
-        name = Resource.objects.get(pk=resource_instance_id).displayname()
-        if remove_prefix:
+        if check_prefix and not self.name.startswith(check_prefix):
+            return
+                                
+        self.notify_groups()
+
+    def get_resource_name(self, remove_prefix=None, remove_suffix=None):
+        name = Resource.objects.get(pk=self.resource_instance_id).displayname()
+
+        if name and remove_prefix:
             name = name.removeprefix(remove_prefix).strip()
-        if remove_suffix:
+        if name and remove_suffix:
             name = name.removesuffix(remove_suffix).strip()
-        return  name, resource_instance_id
+        return  name
     
-    def create_notification(self, message, name, resource_instance_id, response_slug, notiftype_id=None, email=False):
-        self._delete_existing_notification(resource_instance_id)
+    def create_notification(self):
+        self._delete_existing_notification()
+
+        message_template = self.config.get('message', None)
+        message = message_template.format(name=self.name) if message_template else None
+
         notification = models.Notification(
                     message = message,
                     context = {
-                        "resource_instance_id": resource_instance_id,
-                        "resource_id": name,
-                        "response_slug": response_slug
+                        "resource_instance_id": self.resource_instance_id,
+                        "resource_id": self.name,
+                        "response_slug": self.config.get('response_slug', None)
                     },
-                    notiftype_id = notiftype_id
+                    notiftype_id = self.config.get('notiftype_id', None)
                 )
         
-        if email:
+        if self.config.get('email', None):
             link = self._build_url()
             email_params = {
-                "greeting": message,
+                "greeting": self.config.get('message', None),
                 "email": "", 
                 "salutation": "Hi",
                 "username": "", 
@@ -46,34 +61,33 @@ class NotificationStrategy():
         notification.save()
         return notification
     
-    def notify_groups(self, user, group_list, notification):
+    def notify_groups(self):
         from arches_orm.models import Group, Person
         with admin():
+            group_list = self.config.get('groups_to_notify', None)
             for group_id in group_list:
                 group = Group.find(group_id)
                 person_list = [Person.find(member.id) for member in group.members if isinstance(member, Person)]
                 
                 for person in person_list:
-                    if user is None or person != user:
-                        
-                        self.notify_user(person, notification)
+                    if self.user is None or person != self.user:
+                        self.notify_user(person)
         
-    def notify_user(self, user, notification):
-        user = user.user_account
+    def notify_user(self, person):
+        user = person.user_account
 
-        if 'email' in notification.context:
-            notification = self.update_email_details(notification, user)
+        if 'email' in self.notification.context:
+            self.update_email_details(user)
 
         user_x_notification = models.UserXNotification(
-            notif = notification,
+            notif = self.notification,
             recipient = user
         )
         user_x_notification.save()
 
-    def update_email_details(self, notification, user):
-        notification.context['username'] = user.username
-        notification.context['email'] = user.email
-        return notification
+    def update_email_details(self, user):
+        self.notification.context['username'] = user.username
+        self.notification.context['email'] = user.email
     
     def _build_url(self):
         return self.request.build_absolute_uri(f"/index.htm")
@@ -86,9 +100,9 @@ class NotificationStrategy():
         value_string = next((option.get("text").get("en") for option in options if option.get("id") == value_id), None)
         return value_string
 
-    def _delete_existing_notification(self, resource_instance_id):
+    def _delete_existing_notification(self):
         existing_notification = models.Notification.objects.filter(
-            context__resource_instance_id=resource_instance_id
+            context__resource_instance_id=self.resource_instance_id
         ).first()
 
         if existing_notification:
--- coral/functions/notifications/notification_manager.py
+++ coral/functions/notifications/notification_manager.py
@@ -1,20 +1,28 @@
+from coral.functions.notifications.notification_base_strategy import NotificationStrategy
+from arches_orm.adapter import admin
 class NotificationManager():
-    def __init__(self, node_group_id, user, tile, strategy_registry, request):
-        if not strategy_registry:
-            raise ValueError("A strategy register must be provided")
-        self.user = user
-        self.node_group_id = node_group_id
-        self.tile = tile
-        self.strategy_registry = strategy_registry
-        self.strategy = self._select_strategy(node_group_id, request)
-        # self.strategy.delete_existing_notification(tile)
+    def __init__(self, tile, strategy_registry, request):
+        from arches_orm.models import Person
+        with admin():
+            if not strategy_registry:
+                raise ValueError("A strategy register must be provided")
+            self.request = request
+            persons = Person.where(user_account=request.user.id) if request.user else []
+            self.user = persons[0] if persons else None
+            self.tile = tile
+            self.node_group_id = str(tile.nodegroup_id)
+            self.strategy_registry = strategy_registry
+            self.strategy = self._select_strategy()
 
-    def _select_strategy(self, node_group_id, request):
-        strategy_class = self.strategy_registry.get(node_group_id)
+    def _select_strategy(self):
+        settings = self.strategy_registry.get(self.node_group_id)
+        strategy_class = settings.get('strategy', None)
+        config = settings.get('config', None)
+
         if strategy_class:
-            return strategy_class(request)
+            return strategy_class(self.tile, self.request, self.user, config)
         else:
-            raise ValueError(f"No strategy found for node group id: {node_group_id}")
+            return NotificationStrategy(self.tile, self.request, self.user, config)
         
     def notify(self):
-        self.strategy.send_notification(self.user, self.tile)
+        self.strategy.send_notification()
