18218886af4d1439310ec1baf19b308cd6f3c8c1
Stuart Marshall <stuart.marshall@flaxandteal.co.uk>
2025-04-15 13:17:00 +0100

    fix: show no permission if no user is present

--- coral/views/user_to_model.py
+++ coral/views/user_to_model.py
@@ -11,15 +11,16 @@ class UserToModel(View):
 
         from arches_orm.models import Person, Group
         name = None
-        try:
-            with admin():
-                person = Person.where(user_account=int(user.id))
+
+        with admin():
+            person = Person.where(user_account=int(user.id))
+            if person:
                 person = person[0] if len(person) else None 
                 # descriptor was returning as None on arches-orm release/0.2 even though they worked on emerald.
                 # name = person._.resource.descriptors['en']['name']
                 name = person.name[0].full_name
-        except Resource.DoesNotExist:
-            person = None
+            else:
+                person = None
 
         if not person:
             return JSONResponse(
