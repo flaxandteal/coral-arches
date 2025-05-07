dce827307ab1a398d3d5e1021b913a387f312b5a
aidan24 <aidan@galviadigital.com>
2025-05-07 16:23:30 +0100

    feat: added lfs

--- coral/pkg/preliminary_sql/select_sources.sql
+++ coral/pkg/preliminary_sql/select_sources.sql
@@ -1,17 +1,3 @@
-INSERT INTO map_sources(name, source)
-    VALUES ('select-application-area', '{
-        "data": "/geojson?nodeid=1909956f-3a3b-11eb-ae99-f875a44e0e11&include_geojson_link=true",
-        "type": "geojson"
-    }');
-
-INSERT INTO map_sources(name, source)
-    VALUES ('select-area', '{
-        "data": "/geojson?nodeid=64be56e3-3ee5-11eb-b1f0-f875a44e0e11&include_geojson_link=true",
-        "type": "geojson"
-    }');
-
-INSERT INTO map_sources(name, source)
-    VALUES ('select-monument', '{
-        "data": "/geojson?nodeid=ca063178-28cf-11eb-be6d-f875a44e0e11&include_geojson_link=true",
-        "type": "geojson"
-    }');
+version https://git-lfs.github.com/spec/v1
+oid sha256:8d7cbfad54c49554fd2b99def4717f4e96eaf610bd6eaff2d56d0e8f8eba696c
+size 621
