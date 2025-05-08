058b623b48aa8a72833dfa3ea73e45791610d081
aidan24 <aidan@galviadigital.com>
2025-05-08 13:51:58 +0100

    chore: version bump

--- coral/settings.py
+++ coral/settings.py
@@ -23,7 +23,7 @@ except ImportError:
     pass
 
 APP_NAME = 'coral'
-APP_VERSION = semantic_version.Version(major=7, minor=12, patch=42)
+APP_VERSION = semantic_version.Version(major=7, minor=12, patch=43)
 
 GROUPINGS = {
     "groups": {
