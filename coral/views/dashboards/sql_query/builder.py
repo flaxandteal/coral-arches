b15585ad8570b92d77f41d6cb80c6c8218a57563
Stuart Marshall <stuart.marshall@flaxandteal.co.uk>
2025-05-21 16:19:23 +0100

    feat: add validation to sql query and refactor into modules

---
+++ coral/views/dashboards/sql_query/builder.py
@@ -0,0 +1,120 @@
+from .validation import validate_arguments
+
+def build_query(
+    sort_by,
+    count=False,
+    filter={'id':'all', 'type': 'all'},
+    reverse=False,
+    limit=8,
+    offset=0,
+    config=None,
+):
+    validate_arguments(
+        sort_by,
+        count,
+        filter,
+        reverse,
+        limit,
+        offset,
+        config,
+    )
+    return _build_query(
+        sort_by,
+        count,
+        filter,
+        reverse,
+        limit,
+        offset,
+        config
+    )
+
+def _build_query(
+    sort_by,
+    count,
+    filter,
+    reverse,
+    limit,
+    offset,
+    config
+):
+    # config is expected to be a dict with keys: base_sort, sort_options, base_filters, extra_filters
+    base_sort = config['base_sort']
+    sort_options = config['sort_options']
+    base_filters = config['base_filters']
+    extra_filters = config['extra_filters']
+
+    ref_paths = sort_options.get(sort_by, None)
+    order_direction = "DESC" if reverse else "ASC"
+
+    def build_subquery(model_type, filter=None):
+        nodegroupid = ref_paths[model_type]['nodegroupid']
+        sql_path = ref_paths[model_type]['sql']
+        base_filter = base_filters[model_type]
+        if filter and filter['type'] in extra_filters.keys():
+            extra_template = extra_filters[filter['type']][model_type]
+            if not extra_template:
+                return None
+            filter_value = filter['id']
+            extra = extra_filters[filter['type']][model_type].format(filter_value=filter_value)  
+        else:
+            extra = None          
+
+        full_filter = base_filter
+
+        if extra:
+            full_filter = f"({base_filter}) AND ({extra})"
+
+        base_nodegroupid = base_sort[model_type]['nodegroupid']
+
+        if not nodegroupid or not sql_path:
+            # No join, just select NULL for sort_value
+            return f"""
+            SELECT DISTINCT ON (t_fixed.resourceinstanceid)
+                t_fixed.resourceinstanceid,
+                NULL AS sort_value,
+                '{model_type}' AS type
+            FROM tiles t_fixed
+            WHERE t_fixed.nodegroupid = '{base_nodegroupid}'
+            AND {full_filter}
+            """
+        else:
+            return f"""
+            SELECT DISTINCT ON (t_fixed.resourceinstanceid)
+                t_fixed.resourceinstanceid,
+                t_sort.tiledata -> {sql_path} AS sort_value,
+                '{model_type}' AS type
+            FROM tiles t_fixed
+            LEFT JOIN tiles t_sort
+            ON t_fixed.resourceinstanceid = t_sort.resourceinstanceid
+            AND t_sort.nodegroupid = '{nodegroupid}'
+            WHERE t_fixed.nodegroupid = '{base_nodegroupid}'
+            AND {full_filter}
+            """
+
+    if filter['id'] in base_sort.keys():
+        subqueries = [ build_subquery(filter['id']) ]
+    else:
+        subqueries = [
+            build_subquery('Monument', filter),
+            build_subquery('MonumentRevision', filter),
+            build_subquery('Consultation', filter),
+        ]
+
+    if count is True:
+        count_query = f"""
+        SELECT type, COUNT(*) AS count FROM (
+            {' UNION ALL '.join([q for q in subqueries if q])}
+        ) AS main
+        GROUP BY type
+        """
+        return count_query
+
+    full_query = f"""
+    SELECT * FROM (
+        {' UNION ALL '.join([q for q in subqueries if q])}
+    ) AS main
+    ORDER BY sort_value IS NULL, sort_value {order_direction}
+    LIMIT {limit} OFFSET {offset};
+    """
+
+    return full_query
--- coral/views/dashboards/sql_query/config/designation_config.py
+++ coral/views/dashboards/sql_query/config/designation_config.py
@@ -1,6 +1,5 @@
-def build_query(sort_by, count=False, filter={'id':'all', 'type': 'all'}, reverse=False, limit=8, offset=0):
-
-    base_sort = {
+DESIGNATION_SQL_QUERY_CONFIG = {
+    "base_sort": {
         'Monument': {
             'sql': "'325a430a-efe4-11eb-810b-a87eeabdefba' -> 'en' ->> 'value'",
             'nodegroupid': '325a2f2f-efe4-11eb-9b0c-a87eeabdefba'
@@ -13,9 +12,8 @@ def build_query(sort_by, count=False, filter={'id':'all', 'type': 'all'}, revers
             'sql': "'b37552be-9527-11ea-9213-f875a44e0e11' -> 'en' ->> 'value'",
             'nodegroupid': 'b37552ba-9527-11ea-96b5-f875a44e0e11'
         }
-    }
-
-    sort_options = {
+    },
+    "sort_options": {
         'resourceid': {
             'Monument': {
                 'sql': "'325a430a-efe4-11eb-810b-a87eeabdefba' -> 'en' ->> 'value'",
@@ -86,9 +84,8 @@ def build_query(sort_by, count=False, filter={'id':'all', 'type': 'all'}, revers
                 'nodegroupid': ""
             }
         },
-    }
-
-    base_filters = {
+    },
+    "base_filters": {
         'Monument': """
             EXISTS (
                 SELECT 1 FROM tiles t2
@@ -113,9 +110,8 @@ def build_query(sort_by, count=False, filter={'id':'all', 'type': 'all'}, revers
                 AND t2.tiledata ->> '5ffdc00e-03ad-11ef-948f-0242ac150003' IS NULL
             )
         """
-    }
-
-    extra_filters = {
+    },
+    "extra_filters": {
         'council': {
             'Monument': """
                 EXISTS (
@@ -175,81 +171,8 @@ def build_query(sort_by, count=False, filter={'id':'all', 'type': 'all'}, revers
                 """,
             'Consultation': ""
         },
+        'heritage_asset': {},
+        'meetings': {},
+        'revision': {}
     }
-
-    if sort_by not in sort_options:
-        raise ValueError(f"Invalid sort option: {sort_by}")
-
-    ref_paths = sort_options[sort_by]
-    order_direction = "DESC" if reverse else "ASC"
-
-    def build_subquery(model_type, filter=None):
-        nodegroupid = ref_paths[model_type]['nodegroupid']
-        sql_path = ref_paths[model_type]['sql']
-        base_filter = base_filters[model_type]
-        if filter and filter['type'] in extra_filters.keys():
-            filter_value = filter['id']
-            extra = extra_filters[filter['type']][model_type].format(filter_value=filter_value)  
-        else:
-            extra = None          
-
-        full_filter = base_filter
-
-        if extra:
-            full_filter = f"({base_filter}) AND ({extra})"
-
-        base_nodegroupid = base_sort[model_type]['nodegroupid']
-
-        if not nodegroupid or not sql_path:
-            # No join, just select NULL for sort_value
-            return f"""
-            SELECT DISTINCT ON (t_fixed.resourceinstanceid)
-                t_fixed.resourceinstanceid,
-                NULL AS sort_value,
-                '{model_type}' AS type
-            FROM tiles t_fixed
-            WHERE t_fixed.nodegroupid = '{base_nodegroupid}'
-            AND {full_filter}
-            """
-        else:
-            return f"""
-            SELECT DISTINCT ON (t_fixed.resourceinstanceid)
-                t_fixed.resourceinstanceid,
-                t_sort.tiledata -> {sql_path} AS sort_value,
-                '{model_type}' AS type
-            FROM tiles t_fixed
-            LEFT JOIN tiles t_sort
-            ON t_fixed.resourceinstanceid = t_sort.resourceinstanceid
-            AND t_sort.nodegroupid = '{nodegroupid}'
-            WHERE t_fixed.nodegroupid = '{base_nodegroupid}'
-            AND {full_filter}
-            """
-
-    if filter['id'] in base_sort.keys():
-        subqueries = [ build_subquery(filter['id']) ]
-    else:
-        subqueries = [
-            build_subquery('Monument', filter),
-            build_subquery('MonumentRevision', filter),
-            build_subquery('Consultation', filter),
-        ]
-
-    if count is True:
-        count_query = f"""
-        SELECT type, COUNT(*) AS count FROM (
-            {' UNION ALL '.join([q for q in subqueries if q])}
-        ) AS main
-        GROUP BY type
-        """
-        return count_query
-
-    full_query = f"""
-    SELECT * FROM (
-        {' UNION ALL '.join([q for q in subqueries if q])}
-    ) AS main
-    ORDER BY sort_value IS NULL, sort_value {order_direction}
-    LIMIT {limit} OFFSET {offset};
-    """
-    print(full_query)
-
-    return full_query
+}
