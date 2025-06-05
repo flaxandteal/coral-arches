0529e946654d7ff828306d68763bec4bf399da56
Stuart Marshall <stuart.marshall@flaxandteal.co.uk>
2025-06-05 13:40:24 +0100

    chore: version bump v7.13.51

--- coral/settings.py
+++ coral/settings.py
@@ -24,7 +24,7 @@ except ImportError:
     pass
 
 APP_NAME = 'coral'
-APP_VERSION = semantic_version.Version(major=7, minor=13, patch=50)
+APP_VERSION = semantic_version.Version(major=7, minor=13, patch=51)
 
 GROUPINGS = {
     "groups": {
