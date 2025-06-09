dc9c109150062a2229c1fc83aa177c38f8683bde
Stuart Marshall <stuart.marshall@flaxandteal.co.uk>
2025-06-09 12:05:59 +0100

    fix: add default base strategy method in backend for dashboards

--- coral/views/dashboards/base_strategy.py
+++ coral/views/dashboards/base_strategy.py
@@ -1,5 +1,7 @@
 class TaskStrategy():
     def get_tasks(self, groupId, userResourceId, sort_by, sort_order, filter):
         raise NotImplementedError("Subclasses must implement this method")
+    def get_default_sort_order(self):
+        return "desc"
     def build_data(self, resource, groupId):
         raise NotImplementedError("Subclasses must implement this method")
