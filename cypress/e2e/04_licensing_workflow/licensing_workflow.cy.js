7d350604e5f120e4054a8cb220c98153926fdef2
Dylan Cree <dylan.cree@flaxandteal.co.uk>
2025-09-24 14:12:49 +0100

    feat: licensing wf cypress test happy path

--- cypress/e2e/04_licensing_workflow/licensing_workflow.cy.js
+++ cypress/e2e/04_licensing_workflow/licensing_workflow.cy.js
@@ -12,15 +12,12 @@ describe('Going through the licensing Workflow', function () {
         cy.wait(4000);
         cy.contains('Save and Continue').click();
 
-
         cy.get(':nth-child(1) > .workflow-component > .workflow-component-element > .card-component > .new-provisional-edit-card-container > .card > .widgets > :nth-child(1) > :nth-child(1) > .row > .form-group > [style="max-width: 600px; position: relative"] > .col-xs-12 > .form-control').clear('s');
         cy.get(':nth-child(1) > .workflow-component > .workflow-component-element > .card-component > .new-provisional-edit-card-container > .card > .widgets > :nth-child(1) > :nth-child(1) > .row > .form-group > [style="max-width: 600px; position: relative"] > .col-xs-12 > .form-control').type('site test');
         cy.get('.card_component.planning_reference > .row > .form-group > [style="max-width: 600px; position: relative"] > .col-xs-12 > .form-control').clear('t');
         cy.get('.card_component.planning_reference > .row > .form-group > [style="max-width: 600px; position: relative"] > .col-xs-12 > .form-control').type('test ref');
         cy.get('.card_component.cm_reference_number > .row > .form-group > [style="max-width: 600px; position: relative"] > .col-xs-12 > .form-control').clear('c');
         cy.get('.card_component.cm_reference_number > .row > .form-group > [style="max-width: 600px; position: relative"] > .col-xs-12 > .form-control').type('cm num');
-
-        // Selects todo
         cy.get('[aria-label="Applicant, Add new Relationship"]').click();
         cy.wait(2000);
         cy.get('.select2-results__option').first().click();
@@ -50,10 +47,7 @@ describe('Going through the licensing Workflow', function () {
         // cy.wait(2000);
         // cy.get('.select2-results__option').first().click();
         cy.get('[type="radio"]').first().click({force: true});
-        // cy.get('[aria-label="Excavation Type(s), Add new Relationship"]').click();
-        // cy.wait(2000);
         cy.get('.tabbed-workflow-footer-button-container > .btn-success > .verbose').click({force: true});
-
 
         cy.get('.card_component.building_name_value > .row > .form-group > [style="max-width: 600px; position: relative"] > .col-xs-12 > .form-control').clear('t');
         cy.get('.card_component.building_name_value > .row > .form-group > [style="max-width: 600px; position: relative"] > .col-xs-12 > .form-control').type('test building');
@@ -80,78 +74,87 @@ describe('Going through the licensing Workflow', function () {
         cy.get('.col-xs-12 > .form-control').type('test');
         cy.get('.tabbed-workflow-footer-button-container > :nth-child(2)').click({force: true});
 
-        // selects todo
-        // cy.get('#select2-cp0d-container').click();
-        // cy.get('#select2-gbjp-container').click();
-        // cy.get('#select2-nna9-container').click();
-        // cy.get('#select2-k25a-container > .select2-selection__placeholder').click();
-        // cy.get('.card_component.issue_date > .row > .form-group > .col-xs-12 > [style="display: flex; gap: 8px;"] > .input-group > .form-control').click({force: true});
-        // cy.get('.card_component.issue_date > .row > .form-group').click({force: true});
+        cy.get('[aria-label="Cur Grade E Decision, Grant licence"]').click();
+        cy.wait(2000);
+        cy.get('.select2-results__option').first().click();
+        cy.wait(2000);
+        cy.get('[aria-label="Made By, Add new Relationship"]').click({ multiple: true });
+        cy.wait(2000);
+        cy.get('.select2-results__option').first().click();
+        // this select doesn't work for some reason 
+        // cy.get('[aria-label="Cur Grade D Decision, Approved"]').click();
+        // cy.wait(2000);
+        // cy.get('.select2-results__option').first().click();
+        cy.get('[aria-label="Made By, Add new Relationship"]').click({ multiple: true });
+        cy.wait(2000);
+        cy.get('.select2-results__option').first().click();
+        cy.type_ckeditor('editor11', 'test, Description');
 
-        // selects todo
-        // cy.get('#select2-k6p0-container').click();
-        // cy.get('#select2-ivte-container').click();
-        // cy.get('.tabbed-workflow-footer-button-container > .btn-success > .verbose').click({force: true});
-        // cy.get(':nth-child(2) > .verbose').click({force: true});
+        cy.get('.card_component.issue_date > .row > .form-group > .col-xs-12 > [style="display: flex; gap: 8px;"] > .input-group > .form-control').click({force: true});
+        cy.get('.card_component.issue_date > .row > .form-group').click({force: true});
+        cy.get('.tabbed-workflow-footer-button-container > .btn-success > .verbose').click({force: true});
+        cy.get(':nth-child(2) > .verbose').click({force: true});
 
-        // selects todo
-        // cy.get('#select2-26r0-container').click();
-        // cy.get('#select2-n3nu-container').click();
-        // cy.get('#select2-zqax-container > .select2-selection__placeholder').click();
-        // cy.get('.card_component.new_licensee > .widget-wrapper > .rr-widget > .rr-table > [data-bind="foreach: relationshipsInFilter"] > .rr-table-row > .rr-table-row-initial > [style="flex-grow: 1; cursor: pointer;"] > .rr-table-instance-label').click();
-        //selects todo
-        // cy.get('#select2-652p-container').click();
-        // cy.get('#select2-5x5w-container').click();
-        // cy.get('.card_component.date_requested_value > .row > .form-group > .col-xs-12 > [style="display: flex; gap: 8px;"] > .input-group > .form-control').click({force: true});
-        // cy.get('.card_component.date_requested_value > .row').click({force: true});
+        cy.get('[aria-label="Transfer of Licence, Add new Relationship"]').click();
+        cy.wait(2000);
+        cy.get('.select2-results__option').first().click();
+        cy.wait(2000);
+        cy.get('[aria-label="New Applicant, Add new Relationship"]').click({ multiple: true });
+        cy.wait(2000);
+        cy.get('.select2-results__option').first().click();
+        cy.get('[aria-label="Nominated Excavation Director(s), Nominated Excavation Director(s)"]').click({ multiple: true });
+        cy.wait(2000);
+        cy.get('.select2-results__option').first().click();
+        cy.get('[aria-label="Former Employing Body, Add new Relationship"]').click({ multiple: true });
+        cy.wait(2000);
+        cy.get('.select2-results__option').first().click();
+        cy.get('[aria-label="Employing Body/Bodies, Employing Body/Bodies"]').click({ multiple: true });
+        cy.wait(2000);
+        cy.get('.select2-results__option').first().click();
+        cy.get('.card_component.date_requested_value > .row > .form-group > .col-xs-12 > [style="display: flex; gap: 8px;"] > .input-group > .form-control').click({force: true});
+        cy.get('.card_component.date_requested_value > .row').click({force: true});
+        cy.get('[aria-label="Cur Grade E Decision, Do not approve transfer"]').click({ multiple: true });
+        cy.wait(2000);
+        cy.get('.select2-results__option').first().click();
+        cy.get('[aria-label="Made By, Add new Relationship"]').click({ multiple: true });
+        cy.wait(2000);
+        cy.get('.select2-results__option').first().click();
+        cy.get('[aria-label="Cur Grade D Decision, Decline"]').click({ multiple: true });
+        cy.wait(2000);
+        cy.get('.select2-results__option').first().click();
+        cy.get('[aria-label="Made By, Add new Relationship"]').click({ multiple: true }, {force: true});
+        cy.wait(2000);
+        cy.get('.select2-results__option').first().click();
+        cy.get('[aria-label="Made By, Add new Relationship"]').click({ multiple: true }, {force: true});
+        cy.wait(2000);
+        cy.get('.select2-results__option').first().click();
+        cy.get('[aria-label="Made By, Add new Relationship"]').click({ multiple: true }, {force: true});
+        cy.wait(2000);
+        cy.get('.select2-results__option').first().click();
 
-        // selects todo
-        // cy.get('#select2-j7mz-container').click();
-        // cy.get('#select2-0asb-container > .select2-selection__placeholder').click();
-        // cy.get('#select2-eqng-container').click();
-        // cy.get('#select2-hax3-container > .select2-selection__placeholder').click();
-        // cy.get('.card_component.issued_date_value > .row > .form-group > .col-xs-12 > [style="display: flex; gap: 8px;"] > .input-group > .form-control').click({force: true});
-        // cy.get('.card_component.issued_date_value > .row > .form-group > .col-xs-12 > [style="display: flex; gap: 8px;"]').click({force: true});
-        // cy.get('[style="display: flex; justify-content: flex-end; padding: 0 18px;"] > .btn-success').click({force: true});
-        // cy.get(':nth-child(1) > .row > .form-group > .col-xs-12 > .pad-hor > [data-bind="css: { \'active\': value() === false, \'disabled\': disabled }, onEnterkeyClick, onSpacekeyClick, click: function(e){setValue(false)}, attr: {\'aria-checked\': value() === false}"]').click({force: true});
-        // cy.get(':nth-child(2) > .row > .form-group > .col-xs-12 > [style="display: flex; gap: 8px;"] > .input-group > .form-control').click({force: true});
-        // cy.get(':nth-child(2) > .workflow-component > .workflow-component-element').click({force: true});
+        cy.get('.card_component.issued_date_value > .row > .form-group > .col-xs-12 > [style="display: flex; gap: 8px;"] > .input-group > .form-control').click({force: true}, { multiple: true });
+        cy.get('.card_component.issued_date_value > .row > .form-group > .col-xs-12 > [style="display: flex; gap: 8px;"]').click({force: true}, { multiple: true });
+        cy.get('.tabbed-workflow-footer-button-container').contains('Next Step').should('be.visible').click({force: true});
 
-        // selects todo
-        // cy.get('#select2-549f-container').click();
-        // cy.get('#select2-t1vq-container').click();
-        // cy.get('#select2-33qs-container').click();
-        // cy.get('#select2-33ci-container > .select2-selection__placeholder').click();
-        // cy.get(':nth-child(19) > .row > .form-group > .col-xs-12 > [style="display: flex; gap: 8px;"] > .input-group > .form-control').click({force: true});
-        // cy.get(':nth-child(19) > .row > .form-group > .col-xs-12 > [style="display: flex; gap: 8px;"]').click({force: true});
-        // cy.get(':nth-child(20) > .row > .form-group > .col-xs-12 > [style="display: flex; gap: 8px;"] > .input-group > .form-control').click({force: true});
-        // cy.get('[data-day="10/02/2025"]').click();
-        // cy.get('[style="display: flex; justify-content: flex-end; padding: 0 18px;"] > .btn-success').click({force: true});
-        // cy.get('.tabbed-workflow-footer-button-container > .btn-success > .verbose').click({force: true});
-        // cy.get('.col-xs-12 > .form-control').clear('t');
-        // cy.get('.col-xs-12 > .form-control').type('test');
-        // cy.get('.card_component.report_submitted > .row > .form-group > .col-xs-12 > [style="display: flex; gap: 8px;"] > .input-group > .form-control').click({force: true});
-        // cy.get(':nth-child(1) > .workflow-component > .workflow-component-element').click({force: true});
-        //selects todo
-        // cy.get('#select2-m4kt-container').click();
-        // cy.get('.card_component.classification_date_value > .row > .form-group > .col-xs-12 > [style="display: flex; gap: 8px;"] > .input-group > .form-control').click({force: true});
-        // cy.get(':nth-child(1) > .workflow-component > .workflow-component-element > .card-component').click({force: true});
-        // selects todo
-        // cy.get('#select2-1l6c-container').click();
-        // cy.get('.card_component.gis_dataset_received > .row > .form-group > .col-xs-12 > .pad-hor > :nth-child(2)').click({force: true});
-        // cy.get('.card_component.gis_dataset_checked_correct > .row > .form-group > .col-xs-12 > .pad-hor > :nth-child(3)').click({force: true});
-        // cy.get('.card_component.archaeology_found > .row > .form-group > .col-xs-12 > .pad-hor > :nth-child(2)').click({force: true});
-        // cy.get('.card_component.date_reported_value > .row > .form-group > .col-xs-12 > [style="display: flex; gap: 8px;"] > .input-group > .form-control').click({force: true});
-        // cy.get('.card_component.archaeology_found > .row > .form-group > .col-xs-12 > .pad-hor').click({force: true});
-        // cy.get('[style="display: flex; justify-content: flex-end; padding: 0 18px;"] > .btn-success').click({force: true});
-        // cy.get('.tabbed-workflow-footer-button-container > .btn-success').click({force: true});
-        // cy.get('.btn-success').click({force: true});
+        cy.get('.col-xs-12 > .form-control').clear('t');
+        cy.get('.col-xs-12 > .form-control').type('test');
+        cy.get('.card_component.report_submitted > .row > .form-group > .col-xs-12 > [style="display: flex; gap: 8px;"] > .input-group > .form-control').click({force: true});
+        cy.get(':nth-child(1) > .workflow-component > .workflow-component-element').click({force: true});
+        cy.get('[aria-label="Classification Type, Received"]').click({ multiple: true }, {force: true});
+        cy.wait(2000);
+        cy.get('.select2-results__option').first().click();
+        cy.get('.card_component.classification_date_value > .row > .form-group > .col-xs-12 > [style="display: flex; gap: 8px;"] > .input-group > .form-control').click({force: true});
+        cy.get(':nth-child(1) > .workflow-component > .workflow-component-element > .card-component').click({force: true});
+        cy.get('[aria-label="Classified By, Add new Relationship"]').click({ multiple: true }, {force: true});
+        cy.wait(2000);
+        cy.get('.select2-results__option').first().click();
+        cy.get('[type="radio"]').first().click({force: true});
+        cy.get('[type="radio"]').first().click({force: true});
+        cy.get('[type="radio"]').first().click({force: true});
+        cy.get('.card_component.date_reported_value > .row > .form-group > .col-xs-12 > [style="display: flex; gap: 8px;"] > .input-group > .form-control').click({force: true});
+        cy.get('[style="display: flex; justify-content: flex-end; padding: 0 18px;"] > .btn-success').click({force: true}, { multiple: true });
+        cy.get('.tabbed-workflow-footer-button-container > .btn-success').click({force: true}, { multiple: true });
+        cy.wait(2000);
+        cy.get('.btn-success').click({force: true}, { multiple: true });
     })
 })
-
-
-// cy.get('[aria-label="Select Heritage Asset, Please select a Heritage Asset"]').click();
-//         cy.wait(2000);
-//         cy.get('.select2-results__option').contains('HA/01').click();
-//         cy.wait(2000);
-//         cy.contains('Start New').click();
