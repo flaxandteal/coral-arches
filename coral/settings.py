ebdc9f44936e95f6c37f47d0b98c26f759a852dc
Dylan Cree <dylan.cree@flaxandteal.co.uk>
2025-10-09 12:51:13 +0100

    fix: testing ci without hardened cookie settings

--- coral/settings.py
+++ coral/settings.py
@@ -397,12 +397,12 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 15728640
 SESSION_COOKIE_NAME = 'coral'
 
 # Additional cookie security params 
-CSRF_COOKIE_HTTPONLY = False
-CSRF_COOKIE_SAMESITE = "Strict" 
-CSRF_COOKIE_SECURE = True 
-SESSION_COOKIE_HTTPONLY = True 
-SESSION_COOKIE_SAMESITE = "Strict"
-SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_HTTPONLY = False
+# CSRF_COOKIE_SAMESITE = "Strict" 
+# CSRF_COOKIE_SECURE = True 
+# SESSION_COOKIE_HTTPONLY = True 
+# SESSION_COOKIE_SAMESITE = "Strict"
+# SESSION_COOKIE_SECURE = True
 
 # For more info on configuring your cache: https://docs.djangoproject.com/en/2.2/topics/cache/
 CACHES = {
